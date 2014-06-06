c75b4960cb1988cc075976a355e9ec14ba79d34a
James Eaton <james@solidstategroup.com>
2014-06-06 17:14:16 +0100

    Initial Sandpit checkup - code exploring data migration

---
+++ root/database/src/main/resources/fhirnode/fhirnode.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var bodyParser = require('body-parser')
+var handlers = require('./handlers/handlers.js');
+
+var app = express();
+
+app.use(bodyParser());
+
+app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+app.post('/resource', handlers.handle_resource);
+app.get('/resource', handlers.handle_test);
+app.get('/patient/:uuid/observations', handlers.handle_patient_observations);
+app.get('/patient/:uuid', handlers.handle_patient_by_id);
+app.get('/patient*', handlers.handle_patient_by_nhs_number);
+app.all('*', function(req, res) {
+    res.redirect('http://patientview201.apiary-mock.com' + req.url);
+    return;
+});
+
+app.listen(8082);
---
+++ root/database/src/main/resources/fhirnode/handlers/handlers.js
@@ -0,0 +1,188 @@
+var utilities = require('../utilities/utilities.js');
+var pg = require('pg');
+var conString = "postgres://:@localhost/mydb";
+
+function execute_query(sql, param, callback) {
+
+    console.error(JSON.stringify(param));
+
+    var client = new pg.Client(conString);
+    client.connect(function (err) {
+
+        if (err) {
+            return console.error('could not connect to postgres', err);
+        }
+        client.query(sql, param,
+            function (err, result) {
+                client.end();
+
+                if (err) {
+                    console.error('error running query', err);
+                    callback(err, null);
+                } else {
+                    callback(err, result);
+                }
+
+            });
+    });
+    return;
+}
+
+function get_patient_by_id(uuid, callback) {
+
+    var sql = "SELECT ptt.data " +
+              "FROM   fhir.patient_idn idn " +
+              ",      fhir.patient ptt " +
+              "WHERE  ptt._version_id = idn._version_id " +
+              "AND    ptt._logical_id = $1";
+
+    execute_query(sql, [uuid], callback);
+};
+
+
+
+function get_patient_by_nhs_number(nhs_number, callback) {
+
+    var sql =   "SELECT ptt.data " +
+                "FROM   fhir.patient_idn idn " +
+                ",      fhir.patient ptt " +
+                "WHERE  ptt._version_id = idn._version_id " +
+                "AND    idn.label = 'nhsno' " +
+                "AND    idn.value = $1";
+
+    execute_query(sql, [nhs_number], callback);
+};
+
+
+function get_patient_observations_by_name(uuid, name, callback) {
+
+    var sql = "SELECT  ob.data " +
+        "FROM    fhir.obs_subject sub " +
+        ",       fhir.obs ob " +
+        ",       fhir.obs_name obn " +
+        "WHERE   ob._version_id = sub._version_id " +
+        "AND     obn._version_id = ob._version_id " +
+        "AND     sub.display = $1 " +
+        "AND     obn.text = $2 "
+
+    execute_query(sql, [uuid, name], callback);
+};
+
+
+function get_patient_observations(uuid, callback) {
+
+    var sql = "SELECT  ob.data " +
+              "FROM    fhir.obs_subject sub " +
+              ",       fhir.obs ob " +
+              "WHERE   ob._version_id = sub._version_id " +
+              "AND     sub.display = $1"
+
+    execute_query(sql, [uuid], callback);
+};
+
+function load_resource(req, callback) {
+
+    var sql = "SELECT fhir.insert_resource($1::json)";
+    console.log(JSON.stringify(req.body));
+    execute_query(sql,JSON.stringify(req.body), callback);
+
+};
+
+var self = {
+
+    handle_test: function (req, res) {
+        console.log("Manage to handle");
+        utilities.send_success(res, "Test Success");
+    },
+
+    handle_resource: function (req, res) {
+        load_resource(req, function (err, resource) {
+
+            if (err) {
+                console.log(err);
+                utilities.send_failure(res, 500, err);
+                return console.error('error running query', err);
+            } else {
+                console.log('Request successful ' + JSON.stringify(resource.rows[0]));
+            }
+
+            utilities.send_success(res, resource.rows[0]);
+        });
+    },
+
+    handle_patient_by_id: function (req, res) {
+
+        console.log(JSON.stringify(req.params));
+
+        var uuid = req.params.uuid;
+
+        get_patient_by_id(uuid, function (err, patient) {
+
+            if (err) {
+                console.log(err);
+                utilities.send_failure(res, 500, err);
+                return console.error('error running query', err);
+            } else {
+                console.log('Request successful ' + JSON.stringify(patient.rows[0]));
+            }
+
+            utilities.send_success(res, patient.rows[0].data);
+        });
+    },
+
+    handle_patient_by_nhs_number: function (req, res) {
+
+        console.log(JSON.stringify(req.params));
+
+        var nhs_number = req.query.nhsnumber;
+
+        get_patient_by_nhs_number(nhs_number, function (err, patient) {
+
+            if (err) {
+                console.log(err);
+                utilities.send_failure(res, 500, err);
+                return console.error('error running query', err);
+            } else {
+                console.log('Request successful ' + JSON.stringify(patient.rows[0]));
+            }
+
+            utilities.send_success(res, patient.rows[0]);
+        });
+    },
+
+    handle_patient_observations: function (req, res) {
+
+        var uuid = req.params.uuid;
+
+        if (req.query.type) {
+            get_patient_observations_by_name(uuid, req.query.type, function (err, observations) {
+
+                if (err) {
+                    console.log(err);
+                    utilities.send_failure(res, 500, err);
+                    return console.error('error running query', err);
+                } else {
+                    console.log('Request successful ' + JSON.stringify(observations.rows));
+                }
+
+                utilities.send_success(res, observations.rows);
+            });
+        } else {
+            get_patient_observations(uuid, function (err, observations) {
+
+                if (err) {
+                    console.log(err);
+                    utilities.send_failure(res, 500, err);
+                    return console.error('error running query', err);
+                } else {
+                    console.log('Request successful ' + JSON.stringify(observations.rows));
+                }
+
+                utilities.send_success(res, observations.rows);
+            });
+        }
+    },
+
+};
+
+module.exports = self;
