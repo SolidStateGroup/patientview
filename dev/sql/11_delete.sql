aa82e7cf45b82f6b93bc85dac226ffa90867467e
James Eaton <james@solidstategroup.com>
2014-06-09 12:22:37 +0100

    Draft Data Model - cleaning up repo

--- dev/sql/11_delete.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE
-FUNCTION fhir.delete_resource(logical_id uuid)
-  returns void
-  language sql
-  as $$
-    UPDATE fhir.resource SET _state = 'deleted' WHERE _logical_id = delete_resource.logical_id;
-  $$;
