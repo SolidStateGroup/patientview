823eab4b96fbe9791e64d6487d910e3001a0aa2f
jamesrossiter <james@jamesrossiter.co.uk>
2014-06-11 17:17:05 +0100

    frontend - basic user table

--- root/webapp/app/scripts/controllers/staff.js
+++ root/webapp/app/scripts/controllers/staff.js
@@ -1,31 +1,46 @@
 'use strict';
 
-angular.module('patientviewApp').controller('StaffCtrl',['$scope','$timeout', 'CodeService', function ($scope,$timeout,CodeService) {
+angular.module('patientviewApp').controller('StaffCtrl',['$scope','$timeout', 'UserService', 'GroupService',
+    function ($scope,$timeout,UserService,GroupService) {
 
     // Init
     $scope.init = function () {
         $scope.loading = true;
-        $scope.group = {'id':'1234','name':'Example Group'};
-        CodeService.getGroupCodes($scope.group.id).then(function(data) {
-            $scope.editing = false;
-            $scope.list = data;
-            $scope.currentPage = 1; //current page
-            $scope.entryLimit = 2; //max no of items to display in a page
-            $scope.filteredItems = $scope.list.length; //Initially for no filter
-            $scope.totalItems = $scope.list.length;
-            delete $scope.loading;
+        $scope.role = ["1"];
+
+        GroupService.getAll().then(function(data) {
+
+            $scope.allGroups = data;
+
+            UserService.getByRole($scope.role).then(function(data) {
+                $scope.list = data;
+                $scope.currentPage = 1; //current page
+                $scope.entryLimit = 3; //max no of items to display in a page
+                $scope.filteredItems = $scope.list.length; //Initially for no filter
+                $scope.totalItems = $scope.list.length;
+                delete $scope.loading;
+            });
         });
     };
 
-    $scope.opened = function (code, index) {
-        $scope.editcode = _.clone(code);
+    $scope.opened = function (user, index) {
+
+        // create list of available groups (all - users)
+        user.availableGroups = $scope.allGroups;
+
+        for(var i=0;i<user.groups.length;i++) {
+            user.availableGroups = _.without(user.availableGroups, _.findWhere(user.availableGroups, {id: user.groups[i].id}));
+        }
+
+        $scope.edituser = _.clone(user);
+        $scope.groupToAdd = user.availableGroups[0].id;
     };
 
     $scope.add = function (isValid, form, code) {
         if(isValid) {
 
-            CodeService.post($scope.group, code).then(function(addedCode) {
-                $scope.list.push(addedCode);
+            StaffService.post($scope.group, code).then(function(added) {
+                $scope.list.push(added);
                 $scope.newCode = '';
                 $scope.addCodeForm.$setPristine(true);
             }, function() {
@@ -50,11 +65,37 @@ angular.module('patientviewApp').controller('StaffCtrl',['$scope','$timeout', 'C
     };
 
     // Save
-    $scope.save = function (editCodeForm, code, index) {
-        CodeService.save(code).then(function() {
-            editCodeForm.$setPristine(true);
-            $scope.list[index] = _.clone(code);
+    $scope.save = function (editUserForm, user, index) {
+        UserService.save(user).then(function() {
+            editUserForm.$setPristine(true);
+            $scope.list[index] = _.clone(user);
         });
+    };
+
+    // remove group from current groups, add to allowed groups
+    $scope.removeGroup = function (form, user, group) {
+        user.groups = _.without(user.groups, _.findWhere(user.groups, {id: group.id}));
+        user.availableGroups.push(group);
+
+        if (user.availableGroups[0]) {
+            $scope.groupToAdd = user.availableGroups[0].id;
+        }
+
+        form.$setDirty(true);
+    };
+
+    // add group to current group, remove from allowed
+    $scope.addGroup = function (form, user, group) {
+
+        user.availableGroups = _.without(user.availableGroups, _.findWhere(user.availableGroups, {id: group}));
+        user.groups.push(_.findWhere($scope.allGroups, {id: group}));
+
+        if (user.availableGroups[0]) {
+            $scope.groupToAdd = user.availableGroups[0].id;
+        }
+
+        form.$setDirty(true);
+
     };
 
     $scope.init();
