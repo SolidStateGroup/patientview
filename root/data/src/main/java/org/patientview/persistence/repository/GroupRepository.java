da67b65aa4950f68f7c5f0f279d8bde5da8c6cd9
James Eaton <james@solidstategroup.com>
2014-06-17 18:06:30 +0100

    Card #48 added annotation from query

--- root/data/src/main/java/org/patientview/persistence/repository/GroupRepository.java
+++ root/data/src/main/java/org/patientview/persistence/repository/GroupRepository.java
@@ -1,7 +1,10 @@
 package org.patientview.persistence.repository;
 
 import org.patientview.persistence.model.Group;
+import org.patientview.persistence.model.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,4 +16,9 @@ import org.springframework.transaction.annotation.Transactional;
 @Repository
 @Transactional(propagation = Propagation.MANDATORY)
 public interface GroupRepository extends CrudRepository <Group, Long> {
+    @Query("SELECT u " +
+           "FROM User u JOIN u.groupRoles gr " +
+           "WHERE gr.group.id = :groupId AND gr.role.id >=2 ")
+    public Iterable<User> getGroupStaff(@Param(value = "groupId") Long groupId);
+
 }
