9ae70b9039d658279cb0fdbe7d37a5029b97768c
jamesrossiter <james@jamesrossiter.co.uk>
2014-06-17 11:34:41 +0100

    added X-Auth-Token to Access-Control-Allow-Headers in fhirnode.js updated frontend to use angular-local-storage (testing)

--- root/database/src/main/resources/fhirnode/fhirnode.js
+++ root/database/src/main/resources/fhirnode/fhirnode.js
@@ -14,7 +14,7 @@ var dev_port = '8089';
 var enableCORS = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, X-Auth-Token');
 
     // intercept OPTIONS method
     if ('OPTIONS' == req.method) {
