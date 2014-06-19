c5abe23f5842142c5a145374c9ac8844d61e10be
James Eaton <james@solidstategroup.com>
2014-06-19 16:29:57 +0100

    Migration - added pom and patient migration test

---
+++ root/migration/src/test/java/org/patientview/PatientDataMigrationTest.java
@@ -0,0 +1,37 @@
+package org.patientview;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.patientview.migration.service.PatientDataMigrationService;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.inject.Inject;
+
+/**
+ * Unit test for simple App.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(value = "classpath:source-repository.xml")
+public class PatientDataMigrationTest
+{
+
+    @Inject
+    private PatientDataMigrationService patientDataMigrationService;
+
+
+    /**
+     * Order(3) Migrates all the patients into the database
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testPatientMigration() throws Exception {
+        patientDataMigrationService.migrate();
+
+    }
+
+
+
+}
