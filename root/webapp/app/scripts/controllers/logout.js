5e8374341072c83d65856a1ce59f04a8e0cdc4a8
jamesrossiter <james@jamesrossiter.co.uk>
2014-06-23 16:50:45 +0100

    gracefully handle logout errors due to missing tokens

--- root/webapp/app/scripts/controllers/logout.js
+++ root/webapp/app/scripts/controllers/logout.js
@@ -3,5 +3,8 @@
 angular.module('patientviewApp').controller('LogoutCtrl', ['$rootScope', 'AuthService', function ($rootScope, AuthService) {
     AuthService.logout($rootScope.authToken).then(function () {
         $rootScope.logout();
+    }, function () {
+        // failure to log out of server (missing token), log out anyway
+        $rootScope.logout();
     });
 }]);
--- root/webapp/app/scripts/services/authservice.js
+++ root/webapp/app/scripts/services/authservice.js
@@ -14,8 +14,10 @@ angular.module('patientviewApp').factory('AuthService', ['$q', 'Restangular',
         },
         logout: function (token) {
             var deferred = $q.defer();
-            Restangular.all('auth/logout').customDELETE(token).then(function(res) {
-                deferred.resolve(res);
+            Restangular.all('auth/logout').customDELETE(token).then(function(successResult) {
+                deferred.resolve(successResult);
+            }, function (failureResult) {
+                deferred.reject(failureResult);
             });
             return deferred.promise;
         }
