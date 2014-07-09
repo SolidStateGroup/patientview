129e91769cd40176e72f5736b759447117bbe7ec
jamesrossiter <james@jamesrossiter.co.uk>
2014-07-09 16:15:32 +0100

    logic for SPECIALTY_ADMIN, can add children to specialty groups, can create specialty groups and unit groups, cannot edit unit groups (need to consider), GroupService.getAllUnitGroups used by SPECIALTY_ADMIN to get list of all unit groups, (todo: currently just set to get all units)

--- root/webapp/app/scripts/services/groupservice.js
+++ root/webapp/app/scripts/services/groupservice.js
@@ -17,6 +17,13 @@ function ($q, Restangular, UtilService) {
             });
             return deferred.promise;
         },
+        getAllUnitGroups: function () {
+            var deferred = $q.defer();
+            Restangular.all('group').getList().then(function(res) {
+                deferred.resolve(res);
+            });
+            return deferred.promise;
+        },
         getGroupsForUser: function (userId) {
             var deferred = $q.defer();
             // GET /security/user/{userId}/groups
