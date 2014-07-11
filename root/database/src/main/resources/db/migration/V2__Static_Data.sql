eca5e67e0821cd883d5dc8ea5ced63014edce0dc
jamesrossiter <james@jamesrossiter.co.uk>
2014-07-11 12:52:31 +0100

    static data MEMBER

--- root/database/src/main/resources/db/migration/V2__Static_Data.sql
+++ root/database/src/main/resources/db/migration/V2__Static_Data.sql
@@ -43,12 +43,13 @@ INSERT INTO pv_group(id, Group_Name, Code, Description,Type_Id, Visible, Creatio
 INSERT INTO pv_group(id, Group_Name, Code, Description,Type_Id, Visible, Creation_Date,Created_By) VALUES (3, 'Diabetes', 'Diabetes', 'The Diabetes Specialty', 2, true, now(), 1);
 INSERT INTO pv_group(id, Group_Name, Code, Description,Type_Id, Visible, Creation_Date,Created_By) VALUES (4, 'IBD', 'IBD', 'The Inflammatory Bowel Disease Specialty', 2, true, now(), 1);
 
-INSERT INTO pv_role(id, role_name, type_id, level, description, creation_date, created_by) VALUES (1, 'PATIENT', '7', '1', 'A standard patient user', now(), '1');
+INSERT INTO pv_role(id, role_name, type_id, level, description, creation_date, created_by) VALUES (1, 'PATIENT', '7', '2', 'A standard patient user', now(), '1');
 INSERT INTO pv_role(id, role_name, type_id, level, description, creation_date, created_by) VALUES (2, 'UNIT_ADMIN', '6', '4', 'A unit administrator', now(), '1');
 INSERT INTO pv_role(id, role_name, type_id, level, description, creation_date, created_by) VALUES (3, 'STAFF_ADMIN', '6', '3', 'A radar administrator', now(), '1');
 INSERT INTO pv_role(id, role_name, type_id, level, description, creation_date, created_by) VALUES (4, 'SPECIALTY_ADMIN', '6', '5', 'A specialty administrator', now(), '1');
 INSERT INTO pv_role(id, role_name, type_id, level, description, creation_date, created_by) VALUES (5, 'SUPER_ADMIN', '6', '6', 'A superadmin', now(), '1');
 INSERT INTO pv_role(id, role_name, type_id, level, description, creation_date, created_by) VALUES (6, 'GP', '6', '2', 'A doctor', now(), '1');
+INSERT INTO pv_role(id, role_name, type_id, level, description, creation_date, created_by) VALUES (7, 'MEMBER', '7', '1', 'A standard membership', now(), '1');
 
 INSERT INTO pv_user_group_role VALUES(1, 3, 1, 5, now(), null, now(), 1, null, null);
 
