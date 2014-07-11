50e40d16d987921af019dd5d14546dddafde96bf
jamesrossiter <james@jamesrossiter.co.uk>
2014-07-11 15:52:45 +0100

    added identifiers when adding eisting patient, changed generated password length to 8 chars

--- root/webapp/app/scripts/services/utilservice.js
+++ root/webapp/app/scripts/services/utilservice.js
@@ -5,7 +5,7 @@ angular.module('patientviewApp').factory('UtilService', [function () {
         generatePassword: function () {
             var password = '';
             var possible = 'ABCDEFGHKMNPQRSTUVWXYZabcdefghkmnopqrstuvwxyz123456789';
-            for (var k=0;k<5;k++) {
+            for (var k=0;k<9;k++) {
                 password += possible.charAt(Math.floor(Math.random() * possible.length));
             }
             return password;
