ac4a5089947fd2303fa50b95c816bfedbed13f22
James Eaton <james@solidstategroup.com>
2014-07-14 15:50:08 +0100

    Card #137 - CI'd the front end project

--- root/webapp/deploy.sh
+++ root/webapp/deploy.sh
@@ -3,11 +3,28 @@
 #-------------------------------------------------------
 #Author: jamesr@solidstategroup.com
 #Date:   10/06/2014
+#
+#Build script to CI the front end
 #---------------------------------------------------------
 #npm install
-grunt minimal
+
+tomcatUrl=$1
+username=$2
+password=$3
+
+if test $# -ne 3
+then
+	echo "Please supply all paramaters"
+	echo "command [tomcatUrl] [username] [password]"
+	exit 2
+else
+    echo "Deploying to $tomcatUrl";
+fi
+
+
+/usr/local/bin/grunt minimal
 if [ $? -eq 0 ]; then
-    curl -T "dist/webapp.war" "http://tomcat:tomcat@localhost:8080/manager/text/deploy?path=/&update=true"
+    curl -T "dist/webapp.war" "http://$username:$password@$tomcatUrl/manager/text/deploy?path=/&update=true"
     if [ $? -eq 0 ]; then
         echo 'SUCCESS'
     #    exit 0
