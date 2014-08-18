5747529d67f33437f8b1b22d3b99049e7e2c8474
jamesrossiter <james@jamesrossiter.co.uk>
2014-08-18 14:34:15 +0100

    Card #149 - last login to actual date

--- root/data/src/main/java/org/patientview/persistence/model/User.java
+++ root/data/src/main/java/org/patientview/persistence/model/User.java
@@ -87,7 +87,7 @@ public class User extends RangeModel implements UserDetails {
 
     @Column(name = "last_login")
     @Temporal(TemporalType.TIMESTAMP)
-    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="dd-MM-yyyy")
+    //@JsonFormat(shape=JsonFormat.Shape.STRING, pattern="dd-MM-yyyy")
     private Date lastLogin;
 
     @Column(name = "contact_number")
