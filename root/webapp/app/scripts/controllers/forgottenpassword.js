1f5430bfea059087a73aab6f418afbce3f238039
James Eaton <james@solidstategroup.com>
2014-08-20 13:38:51 +0100

    Quick fix for success message

--- root/webapp/app/scripts/controllers/forgottenpassword.js
+++ root/webapp/app/scripts/controllers/forgottenpassword.js
@@ -8,12 +8,10 @@ angular.module('patientviewApp').controller('ForgottenPasswordCtrl', ['RouteServ
         $scope.submit = function () {
             AuthService.forgottenPassword($scope.credentials).then(function () {
                 // successfully changed user password
+                $scope.successMessage = '- Your new password has been sent to your email address. When you receive it you can use it to log on. After logging on you will be asked to change your password.';
             }, function (result) {
                 if (result.status === 404) {
                     $scope.errorMessage = '- The account could not be found';
-                }
-                if (result.status === 201) {
-                    $scope.successMessage = '- Your new password has been sent to your email address. When you receive it you can use it to log on. After logging on you will be asked to change your password.';
                 }
             });
 
