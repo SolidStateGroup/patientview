d8343cd25b7dff81a87f53a6f7c45bc6f1a07380
jamesrossiter <james@jamesrossiter.co.uk>
2014-08-28 15:56:34 +0100

    remove comment

--- root/api/src/main/java/org/patientview/api/service/UserService.java
+++ root/api/src/main/java/org/patientview/api/service/UserService.java
@@ -38,7 +38,6 @@ public interface UserService extends CrudService<User> {
     @AuditTrail(value = AuditActions.VIEW, objectType = User.class)
     User get(Long userId);
 
-    //Page<User> getUsersByGroupsAndRoles(List<Long> groupIds, List<Long> roleIds, GetParameters getParameters);
     Page<User> getUsersByGroupsAndRoles(GetParameters getParameters);
 
     @AuditTrail(value = AuditActions.CHANGE_PASSWORD, objectType = User.class)
