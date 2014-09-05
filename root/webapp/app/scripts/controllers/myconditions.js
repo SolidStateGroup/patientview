6bae38458f7a3aa26d7694ea67648b1edf04f120
jamesrossiter <james@jamesrossiter.co.uk>
2014-09-05 12:25:42 +0100

    Card #155 My Conditions stub

---
+++ root/webapp/app/scripts/controllers/myconditions.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('patientviewApp').controller('MyconditionsCtrl',['$scope', 'PatientService', 'UserService',
+function ($scope, PatientService, UserService) {
+
+    $scope.init = function(){
+
+    };
+
+    $scope.init();
+}]);
