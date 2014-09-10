d6c306e8778eaa6d2c13a327ed61b346ae272968
jamesrossiter <james@jamesrossiter.co.uk>
2014-09-10 11:07:33 +0100

    add http:// to links on my conditions where not present

---
+++ root/webapp/app/scripts/filters/addhttpfilter.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// add http filter (adds http:// to strings without http:// present)
+angular.module('patientviewApp').filter('addhttpFilter', [function () {
+    return function (text) {
+        if (text.indexOf('http://') > -1) {
+            return text;
+        } else {
+            return 'http://' + text;
+        }
+    };
+}]);
