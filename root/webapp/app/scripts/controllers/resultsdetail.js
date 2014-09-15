a6bc6d68e7a5105ce343e046ffd032a20194a227
jamesrossiter <james@jamesrossiter.co.uk>
2014-09-15 13:15:56 +0100

    Card #164 initial stub for result details

--- root/webapp/app/scripts/controllers/resultsdetail.js
+++ root/webapp/app/scripts/controllers/resultsdetail.js
@@ -1,5 +1,42 @@
 'use strict';
 
-angular.module('patientviewApp').controller('ResultsDetailCtrl',['$scope', function ($scope) {
+angular.module('patientviewApp').controller('ResultsDetailCtrl',['$scope', '$routeParams', '$location', 'ObservationHeadingService', 'ObservationService',
+function ($scope, $routeParams, $location, ObservationHeadingService, ObservationService) {
 
+    $scope.init = function() {
+        var i;
+
+        // if query parameters not set redirect to results
+        if ($routeParams.code === undefined) {
+            $location.path('/results');
+        }
+
+        // handle single result type from query parameter, todo: multiple result types
+        $scope.code = $routeParams.code;
+
+        if ($scope.code instanceof Array) {
+            $scope.code = $scope.code[0];
+        }
+
+        $scope.getObservationHeadings();
+        $scope.getObservations();
+    };
+
+    $scope.getObservationHeadings = function() {
+        ObservationHeadingService.getAll().then(function(observationHeadings) {
+            console.log(observationHeadings);
+        }, function() {
+            alert('Error retrieving results');
+        });
+    };
+
+    $scope.getObservations = function() {
+        ObservationService.getByCode($scope.loggedInUser.id, $scope.code).then(function(observations) {
+            console.log(observations);
+        }, function() {
+            alert('Error retrieving results');
+        });
+    };
+
+    $scope.init();
 }]);
--- root/webapp/app/scripts/services/observationservice.js
+++ root/webapp/app/scripts/services/observationservice.js
@@ -1,51 +1,50 @@
 'use strict';
 
-angular.module('patientviewApp').factory('ObservationService', ['$q', 'Restangular',
-    function ($q, Restangular) {
-        return {
-            getObservation: function (uuid, typeName) {
-                var deferred = $q.defer();
-                Restangular.one('patient',uuid).getList('observations', {type: typeName}).then(function(res) {
-                    deferred.resolve(res);
-                });
-                return deferred.promise;
-            },
-            getResultTypes: function (uuid) {
-                var deferred = $q.defer();
-                Restangular.one('patient',uuid).getList('resulttypes').then(function(res) {
-                    deferred.resolve(res);
-                });
-                return deferred.promise;
-            },
-            getAll: function (userId) {
-                var deferred = $q.defer();
-                // GET /user/{userId}/observations
-                Restangular.one('user', userId).one('observations').get().then(function(successResult) {
-                    deferred.resolve(successResult);
-                }, function(failureResult) {
-                    deferred.reject(failureResult);
-                });
-                return deferred.promise;
-            },
-            getByCode: function (userId, code) {
-                var deferred = $q.defer();
-                // GET /user/{userId}/observations/{code}
-                Restangular.one('user', userId).one('observations').one(code).get().then(function(successResult) {
-                    deferred.resolve(successResult);
-                }, function(failureResult) {
-                    deferred.reject(failureResult);
-                });
-                return deferred.promise;
-            },
-            getSummary: function (userId) {
-                var deferred = $q.defer();
-                // GET /user/{userId}/observations/summary
-                Restangular.one('user', userId).one('observations').one('summary').get().then(function(successResult) {
-                    deferred.resolve(successResult);
-                }, function(failureResult) {
-                    deferred.reject(failureResult);
-                });
-                return deferred.promise;
-            }
-        };
-    }]);
+angular.module('patientviewApp').factory('ObservationService', ['$q', 'Restangular', function ($q, Restangular) {
+    return {
+        getObservation: function (uuid, typeName) {
+            var deferred = $q.defer();
+            Restangular.one('patient',uuid).getList('observations', {type: typeName}).then(function(res) {
+                deferred.resolve(res);
+            });
+            return deferred.promise;
+        },
+        getResultTypes: function (uuid) {
+            var deferred = $q.defer();
+            Restangular.one('patient',uuid).getList('resulttypes').then(function(res) {
+                deferred.resolve(res);
+            });
+            return deferred.promise;
+        },
+        getAll: function (userId) {
+            var deferred = $q.defer();
+            // GET /user/{userId}/observations
+            Restangular.one('user', userId).one('observations').get().then(function(successResult) {
+                deferred.resolve(successResult);
+            }, function(failureResult) {
+                deferred.reject(failureResult);
+            });
+            return deferred.promise;
+        },
+        getByCode: function (userId, code) {
+            var deferred = $q.defer();
+            // GET /user/{userId}/observations/{code}
+            Restangular.one('user', userId).one('observations').one(code).get().then(function(successResult) {
+                deferred.resolve(successResult);
+            }, function(failureResult) {
+                deferred.reject(failureResult);
+            });
+            return deferred.promise;
+        },
+        getSummary: function (userId) {
+            var deferred = $q.defer();
+            // GET /user/{userId}/observations/summary
+            Restangular.one('user', userId).one('observations').one('summary').get().then(function(successResult) {
+                deferred.resolve(successResult);
+            }, function(failureResult) {
+                deferred.reject(failureResult);
+            });
+            return deferred.promise;
+        }
+    };
+}]);
