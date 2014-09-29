c46867a816a5524035f2af33e2182d9a4c8e8b73
James Rossiter <james[at]jamesrossiter.co.uk>
2014-09-29 12:10:23 +0100

    Card #139 consolidation of migration tests to support ordered execution (Suite doesn't allow dependency injection in this case)

--- root/migration/src/test/java/org/patientview/MigrationIntegrationTest.java
+++ root/migration/src/test/java/org/patientview/MigrationIntegrationTest.java
@@ -1,10 +1,15 @@
 package org.patientview;
 
 import org.junit.Assert;
+import org.junit.FixMethodOrder;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.patientview.enums.Roles;
 import org.patientview.migration.service.AdminDataMigrationService;
+import org.patientview.migration.service.PatientDataMigrationService;
+import org.patientview.migration.service.UserDataMigrationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.test.annotation.Rollback;
@@ -14,20 +19,25 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.inject.Inject;
 
-
 /**
- * Created by james@solidstategroup.com
- * Created on 04/06/2014
+ * Created by jamesr@solidstategroup.com
+ * Created on 29/09/2014
  */
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(value = "classpath:source-repository.xml")
-public class AdminDataMigrationTest {
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class MigrationIntegrationTest {
 
-    private static final Logger LOG = LoggerFactory.getLogger(AdminDataMigrationTest.class);
+    private static final Logger LOG = LoggerFactory.getLogger(MigrationIntegrationTest.class);
 
     @Inject
     private AdminDataMigrationService adminDataMigrationService;
+
+    @Inject
+    private UserDataMigrationService userDataMigrationService;
+
+    @Inject
+    private PatientDataMigrationService patientDataMigrationService;
 
     /**
      * Order(1) Migrates all the static data like groups, specialities
@@ -37,7 +47,7 @@ public class AdminDataMigrationTest {
     @Test
     @Transactional
     @Rollback(false)
-    public void testStaticDataMigrationFeatures()  throws Exception {
+    public void test01StaticDataMigrationFeatures()  throws Exception {
         LOG.info("Starting migration");
         adminDataMigrationService.migrate();
         Assert.assertNotNull("This group should not be null", adminDataMigrationService.getLookupByName("UNIT"));
@@ -47,5 +57,27 @@ public class AdminDataMigrationTest {
         Assert.assertNotNull("This feature should not be null", adminDataMigrationService.getRoleByName(Roles.STAFF_ADMIN));
     }
 
+    /**
+     * Order(2) Migrates all the user records that are patients with groups into the new schema
+     *
+     * @throws Exception
+     */
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void test02UserMigration() {
+        userDataMigrationService.migrate();
+    }
 
+    /**
+     * Order(3) Migrates all the patients into the database
+     *
+     * @throws Exception
+     */
+    @Test
+    @Ignore
+    public void test03PatientMigration() throws Exception {
+        patientDataMigrationService.migrate();
+
+    }
 }
