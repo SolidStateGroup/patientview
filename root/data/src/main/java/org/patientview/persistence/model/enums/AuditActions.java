ddfd2d15444238e19ee5ebcfbfe15b4f18524b3a
James Rossiter <james@jamesrossiter.co.uk>
2014-10-06 16:06:22 +0100

    hide medications filter if none available

--- root/data/src/main/java/org/patientview/persistence/model/enums/AuditActions.java
+++ root/data/src/main/java/org/patientview/persistence/model/enums/AuditActions.java
@@ -6,9 +6,18 @@ package org.patientview.persistence.model.enums;
  */
 public enum AuditActions {
 
-    CREATE("Create"), EDIT("Edit"), VIEW("View"), CHANGE_PASSWORD("Change Password"), DELETE("Delete User"),
-    RESET_PASSWORD("Reset Password"), VERIFY_EMAIL("Verify Email"), LOGON_SUCCESS("Logon Success"), MAP("Map"),
-    LOGON_FAIL("Logon failed"), LOGOFF("log off"), SWITCH_USER("Switch User");
+    CREATE("Create"),
+    EDIT("Edit"),
+    VIEW("View"),
+    CHANGE_PASSWORD("Change Password"),
+    DELETE("Delete User"),
+    RESET_PASSWORD("Reset Password"),
+    VERIFY_EMAIL("Verify Email"),
+    LOGON_SUCCESS("Logon Success"),
+    MAP("Map"),
+    LOGON_FAIL("Logon failed"),
+    LOGOFF("log off"),
+    SWITCH_USER("Switch User");
 
     private String name;
     AuditActions(String name) { this.name = name; }
