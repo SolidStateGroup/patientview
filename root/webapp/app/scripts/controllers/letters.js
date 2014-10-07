6b025480f0286758e5366ebe782619f35a0b28af
James Rossiter <james@jamesrossiter.co.uk>
2014-10-07 13:58:35 +0100

    Card #167 UI for letters (basic table, to discuss)

--- root/webapp/app/scripts/controllers/letters.js
+++ root/webapp/app/scripts/controllers/letters.js
@@ -1,5 +1,50 @@
 'use strict';
+// view letter modal instance controller
+var ViewLetterModalInstanceCtrl = ['$scope', '$modalInstance', 'letter',
+function ($scope, $modalInstance, letter) {
+    $scope.letter = letter;
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+}];
 
-angular.module('patientviewApp').controller('LettersCtrl',['$scope', function ($scope) {
+angular.module('patientviewApp').controller('LettersCtrl', ['$scope', '$modal', 'LetterService',
+function ($scope, $modal, LetterService) {
+    var init = function(){
+        $scope.loading = true;
 
+        LetterService.getByUserId($scope.loggedInUser.id).then(function(letters) {
+            $scope.letters = letters;
+            $scope.predicate = 'date';
+            $scope.reverse = true;
+            $scope.loading = false;
+        }, function () {
+            alert('Cannot get letters');
+            $scope.loading = false;
+        })
+    };
+
+    // open modal
+    $scope.viewLetter = function(letter) {
+        $scope.successMessage = '';
+
+        var modalInstance = $modal.open({
+            templateUrl: 'views/partials/viewLetterModal.html',
+            controller: ViewLetterModalInstanceCtrl,
+            size: 'lg',
+            resolve: {
+                letter: function(){
+                    return letter;
+                }
+            }
+        });
+
+        modalInstance.result.then(function () {
+            // ok (not used)
+        }, function () {
+            // closed
+        });
+    };
+
+    init();
 }]);
