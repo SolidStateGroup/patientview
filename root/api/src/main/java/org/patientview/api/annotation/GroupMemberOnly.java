be22083a0a8cd0ad9d83f0bc220c9b34a607a08f
James Rossiter <james@jamesrossiter.co.uk>
2014-10-08 17:49:35 +0100

    Added checkstyle to API, will continue to build on fail and copy to console, copied definitions from patientview1 (to review, consider failing build on failed checkstyle)

--- root/api/src/main/java/org/patientview/api/annotation/GroupMemberOnly.java
+++ root/api/src/main/java/org/patientview/api/annotation/GroupMemberOnly.java
@@ -14,5 +14,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface GroupMemberOnly {
-    RoleName[] roles() default {};
+    RoleName[] roles() default { };
 }
