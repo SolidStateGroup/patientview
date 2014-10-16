38ee17dcacdc558d427f250ccd924822e259bd82
James Rossiter <james@jamesrossiter.co.uk>
2014-10-16 15:42:43 +0100

    Card #242 staff and patient specific help

--- root/webapp/app/scripts/controllers/help.js
+++ root/webapp/app/scripts/controllers/help.js
@@ -2,4 +2,11 @@
 
 angular.module('patientviewApp').controller('HelpCtrl',['$scope', function ($scope) {
 
+    // set staff or patient based on user information
+    if ($scope.loggedInUser.userInformation.staffFeatures) {
+        $scope.staffUser = true;
+    } else {
+        $scope.patientUser = true;
+    }
+
 }]);
