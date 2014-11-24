f00c361ffd9ced86ceb88f9739f893ce7af60377
James Rossiter <james@jamesrossiter.co.uk>
2014-11-24 13:30:21 +0000

    Card #61 GP Medicines, add optInStatus, optInHidden, optOutHidden, optInDate to UserFeature, ALTER TABLE "public"."pv_feature_user" ADD COLUMN "opt_in_status" BOOLEAN; ALTER TABLE "public"."pv_feature_user" ADD COLUMN "opt_in_hidden" BOOLEAN; ALTER TABLE "public"."pv_feature_user" ADD COLUMN "opt_out_hidden" BOOLEAN; ALTER TABLE "public"."pv_feature_user" ADD COLUMN "opt_in_date" TIMESTAMP WITHOUT TIME ZONE;

--- root/common/src/main/java/org/patientview/persistence/model/UserFeature.java
+++ root/common/src/main/java/org/patientview/persistence/model/UserFeature.java
@@ -3,11 +3,15 @@ package org.patientview.persistence.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
 
 /**
  * Created by james@solidstategroup.com
@@ -25,6 +29,19 @@ public class UserFeature extends RangeModel {
     @OneToOne(cascade = CascadeType.MERGE)
     @JoinColumn(name = "feature_id", nullable = false)
     private Feature feature;
+
+    @Column(name = "opt_in_status")
+    private Boolean optInStatus;
+
+    @Column(name = "opt_in_hidden")
+    private Boolean optInHidden;
+
+    @Column(name = "opt_out_hidden")
+    private Boolean optOutHidden;
+
+    @Column(name = "opt_in_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date optInDate;
 
     public UserFeature () {
     }
@@ -50,5 +67,35 @@ public class UserFeature extends RangeModel {
         this.user = user;
     }
 
+    public Boolean getOptInStatus() {
+        return optInStatus;
+    }
 
+    public void setOptInStatus(Boolean optInStatus) {
+        this.optInStatus = optInStatus;
+    }
+
+    public Boolean getOptInHidden() {
+        return optInHidden;
+    }
+
+    public void setOptInHidden(Boolean optInHidden) {
+        this.optInHidden = optInHidden;
+    }
+
+    public Boolean getOptOutHidden() {
+        return optOutHidden;
+    }
+
+    public void setOptOutHidden(Boolean optOutHidden) {
+        this.optOutHidden = optOutHidden;
+    }
+
+    public Date getOptInDate() {
+        return optInDate;
+    }
+
+    public void setOptInDate(Date optInDate) {
+        this.optInDate = optInDate;
+    }
 }
