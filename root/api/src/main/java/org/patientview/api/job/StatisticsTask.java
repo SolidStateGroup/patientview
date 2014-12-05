79c991403718e87d5823d72d391b32ac96939b41
James Rossiter <james@jamesrossiter.co.uk>
2014-12-05 10:14:54 +0000

    Statistics: set to every 10 minutes instead of 5

--- root/api/src/main/java/org/patientview/api/job/StatisticsTask.java
+++ root/api/src/main/java/org/patientview/api/job/StatisticsTask.java
@@ -57,9 +57,9 @@ public class StatisticsTask {
     }
 
     /**
-     * Statistics for the current month, run every 5 minutes
+     * Statistics for the current month, run every 10 minutes
      */
-    @Scheduled(cron = "0 */5 * * * ?") // every five minutes
+    @Scheduled(cron = "0 */10 * * * ?") // every 10 minutes
     //@Scheduled(cron = "0 0 1 * * ?") // every day at 01:00
     public void generateThisMonthStatistics() {
         Calendar calendar = timer.getCurrentDate();
@@ -75,7 +75,7 @@ public class StatisticsTask {
         calendar.set(Calendar.MILLISECOND, 0);
         Date startDate = calendar.getTime();
 
-        LOG.info("Creating statistics (every 5m) for period " + startDate.toString() + " to " + endDate.toString());
+        LOG.info("Creating statistics (every 10m) for period " + startDate.toString() + " to " + endDate.toString());
         groupStatisticService.generateGroupStatistic(startDate, endDate, StatisticPeriod.MONTH);
     }
 }
