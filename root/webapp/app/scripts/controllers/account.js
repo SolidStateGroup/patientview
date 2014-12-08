d9ee0fdf19f3d959a7df3392e6cded3d7e56bf3b
James Rossiter <james@jamesrossiter.co.uk>
2014-12-08 14:55:08 +0000

    Card #275 UAT - password and settings alert positions

--- root/webapp/app/scripts/controllers/account.js
+++ root/webapp/app/scripts/controllers/account.js
@@ -16,18 +16,19 @@ angular.module('patientviewApp').controller('AccountCtrl', ['localStorageService
 
     $scope.saveSettings = function () {
         // If the email field has been changed validate emails
-        $scope.successMessage = null;
-        $scope.errorMessage = null;
+        $scope.settingsSuccessMessage = null;
+        $scope.settingsErrorMessage = null;
+
         if (!$scope.userdetails.confirmEmail) {
-            $scope.errorMessage = 'Please confirm the email address';
+            $scope.settingsErrorMessage = 'Please confirm the email address';
         } else {
             // Email equal and correct
             if (($scope.userdetails.confirmEmail === $scope.userdetails.email)) {
                 if (UtilService.validateEmail($scope.userdetails.email)) {
-                    $scope.errorMessage = 'Invalid format for email';
+                    $scope.settingsErrorMessage = 'Invalid format for email';
                 } else {
                     UserService.saveOwnSettings($scope.loggedInUser.id, $scope.userdetails).then(function () {
-                        $scope.successMessage = 'The settings have been saved';
+                        $scope.settingsSuccessMessage = 'The settings have been saved';
                         AuthService.getUserInformation($scope.loggedInUser.userInformation.token)
                             .then(function (userInformation) {
 
@@ -41,40 +42,38 @@ angular.module('patientviewApp').controller('AccountCtrl', ['localStorageService
 
                         }, function(result) {
                             if (result.data) {
-                                $scope.errorMessage = result.data;
+                                $scope.settingsErrorMessage = result.data;
                             } else {
-                                delete $scope.errorMessage;
+                                delete $scope.settingsErrorMessage;
                             }
                             $scope.loading = false;
                         });
 
                     }, function (result) {
-                        $scope.errorMessage = 'The settings have not been saved ' + result.data;
+                        $scope.settingsErrorMessage = 'The settings have not been saved ' + result.data;
                     });
                 }
             } else {
-                $scope.errorMessage = 'The emails do not match';
+                $scope.settingsErrorMessage = 'The emails do not match';
             }
         }
     };
 
     $scope.savePassword = function () {
-        $scope.successMessage = null;
+        $scope.passwordSuccessMessage = null;
         $scope.passwordErrorMessage = null;
+
         if ($scope.pw !== $scope.userdetails.confirmPassword) {
             $scope.passwordErrorMessage = 'The passwords do not match';
         } else {
             AuthService.login({'username': $scope.userdetails.username, 'password': $scope.userdetails.currentPassword}).then(function () {
-                // set the password
                 $scope.userdetails.password =  $scope.pw;
 
                 UserService.changePassword($scope.userdetails).then(function () {
-                    // successfully changed user password
-                    $scope.successMessage = 'The password has been saved';
+                    $scope.passwordSuccessMessage = 'The password has been saved';
                 }, function () {
                     $scope.passwordErrorMessage = 'There was an error';
                 });
-
             }, function (result) {
                 if (result.data) {
                     $scope.passwordErrorMessage = 'Current password incorrect';
