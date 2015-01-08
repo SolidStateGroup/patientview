dab45f8a20e187458f1404f76c1bf70ae46886e8
James Rossiter <james@jamesrossiter.co.uk>
2015-01-08 12:35:43 +0000

    default messaging contacts post migration SQL

--- root/database/src/main/resources/db/migration/V5__Post_Migration.sql
+++ root/database/src/main/resources/db/migration/V5__Post_Migration.sql
@@ -1,4 +1,6 @@
-INSERT INTO "pv_result_cluster" ( "id", "name") 
+
+/* result clusters */
+INSERT INTO "pv_result_cluster" ( "id", "name")
 VALUES (1 , 'Blood Pressure, Glucose, Weight' );
 INSERT INTO "pv_result_cluster" ( "id", "name") 
 VALUES (2 , 'U + E test' );
@@ -22,3 +24,27 @@ INSERT INTO "pv_result_cluster_observation_heading" ( "display_order", "id", "ob
 VALUES ( 4, 8, (SELECT id FROM pv_observation_heading WHERE pv_observation_heading.code ='creatinine'),  2);
 INSERT INTO "pv_result_cluster_observation_heading" ( "display_order", "id", "observation_heading_id", "result_cluster_id") 
 VALUES ( 5, 9, (SELECT id FROM pv_observation_heading WHERE pv_observation_heading.code ='hco3'),  2);
+
+/* default messaging contacts (function must be created as postgres) */
+CREATE OR REPLACE FUNCTION featureIDdmc() RETURNS INT AS
+$BODY$
+DECLARE feature_id1 INT;
+BEGIN
+ SELECT ID FROM pv_feature WHERE feature_name = 'DEFAULT_MESSAGING_CONTACT' INTO feature_id1;
+ return feature_id1;
+END
+$BODY$
+LANGUAGE plpgsql;
+
+INSERT INTO pv_feature_user (feature_id, user_id, creation_date, created_by, id)
+SELECT DISTINCT featureIDdmc(), pv_user.id, now(), 2, nextval( 'hibernate_sequence' )
+FROM pv_contact_point
+JOIN pv_group ON group_id = pv_group.id
+JOIN pv_user_group_role ON pv_user_group_role.group_id = pv_group.id
+JOIN pv_user ON pv_user.id = pv_user_group_role.user_id
+WHERE upper(pv_user.email) = upper(pv_contact_point.content)
+AND pv_contact_point.type_id IN (SELECT id FROM pv_lookup_value WHERE VALUE = 'PV_ADMIN_EMAIL')
+AND pv_group.type_id = 1
+AND pv_user.id NOT IN (
+SELECT user_id FROM pv_feature_user WHERE feature_id = featureIDdmc()
+);
