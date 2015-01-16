9929413433705e8af1aabbb3254f67db39746118
James Rossiter <james@jamesrossiter.co.uk>
2015-01-16 14:16:30 +0000

    Importer: fix for wiping transplant status

--- root/api/src/main/java/org/patientview/api/job/UktTask.java
+++ root/api/src/main/java/org/patientview/api/job/UktTask.java
@@ -33,8 +33,8 @@ public class UktTask {
     /**
      * Import UKT data, wiping out existing database contents
      */
-    @Scheduled(cron = "0 */1 * * * ?") // every minute
-    //@Scheduled(cron = "0 0 2 * * ?") // every day at 02:00
+    //@Scheduled(cron = "0 */1 * * * ?") // every minute
+    @Scheduled(cron = "0 0 2 * * ?") // every day at 02:00
     public void importUktData() throws ResourceNotFoundException, FhirResourceException, UktException {
         Boolean importEnabled = Boolean.parseBoolean(properties.getProperty("ukt.import.enabled"));
 
