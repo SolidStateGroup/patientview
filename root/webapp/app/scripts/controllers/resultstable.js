746b65b788962f4516e6431d45768b671eca9308
James Rossiter <james@jamesrossiter.co.uk>
2015-02-02 15:46:58 +0000

    Card #318, results table now uses name instead of heading for display and in drop down list, full pagination with first/last etc

--- root/webapp/app/scripts/controllers/resultstable.js
+++ root/webapp/app/scripts/controllers/resultstable.js
@@ -172,50 +172,73 @@ function ($scope, $modal, $filter, ObservationService, ObservationHeadingService
 
     // pagination
     $scope.pageCount = function() {
-        return Math.ceil($scope.total/$scope.itemsPerPage);
+        return Math.ceil($scope.total / $scope.itemsPerPage);
     };
+
     $scope.range = function() {
-        var rangeSize = 10;
-        var ret = [];
-        var start;
+        var rangeSize = $scope.itemsPerPage;
+        var pageNumbers = [];
+        var startPage;
 
-        if ($scope.currentPage < 10) {
-            start = 0;
+        if (($scope.currentPage - $scope.itemsPerPage / 2) < 0) {
+            startPage = 0;
         } else {
-            start = $scope.currentPage;
+            startPage = $scope.currentPage - $scope.itemsPerPage / 2;
         }
 
-        if ( start > $scope.pageCount()-rangeSize ) {
-            start = $scope.pageCount()-rangeSize;
+        if (startPage > $scope.pageCount() - rangeSize) {
+            startPage = $scope.pageCount() - rangeSize;
         }
 
-        for (var i=start; i<start+rangeSize; i++) {
+        for (var i = startPage; i < startPage + rangeSize; i++) {
             if (i > -1) {
-                ret.push(i);
+                pageNumbers.push(i);
             }
         }
-        return ret;
+
+        return pageNumbers;
     };
-    $scope.setPage = function(n) {
-        if (n > -1 && n < $scope.totalPages) {
-            $scope.currentPage = n;
+
+    $scope.setPage = function(pageNumber) {
+        if (pageNumber > -1 && pageNumber < $scope.totalPages) {
+            $scope.currentPage = pageNumber;
         }
     };
+
+    $scope.firstPage = function() {
+        $scope.currentPage = 0;
+    };
+
     $scope.prevPage = function() {
         if ($scope.currentPage > 0) {
             $scope.currentPage--;
         }
     };
-    $scope.prevPageDisabled = function() {
-        return $scope.currentPage === 0 ? 'hidden' : '';
-    };
+
     $scope.nextPage = function() {
         if ($scope.currentPage < $scope.totalPages - 1) {
             $scope.currentPage++;
         }
     };
+
+    $scope.lastPage = function() {
+        $scope.currentPage = $scope.totalPages - 1;
+    };
+
+    $scope.firstPageDisabled = function() {
+        return (($scope.currentPage - $scope.itemsPerPage / 2) < 0) ? 'hidden' : '';
+    };
+
+    $scope.prevPageDisabled = function() {
+        return $scope.currentPage === 0 ? 'hidden' : '';
+    };
+
     $scope.nextPageDisabled = function() {
-        return $scope.currentPage === $scope.pageCount() - 1 ? 'disabled' : '';
+        return $scope.currentPage === $scope.pageCount() - 1 ? 'hidden' : '';
+    };
+
+    $scope.lastPageDisabled = function() {
+        return ($scope.currentPage + 6 > $scope.pageCount()) ? 'hidden' : '';
     };
 
     $scope.init();
