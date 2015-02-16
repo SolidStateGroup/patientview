4de90d3c1c32f561c715b1e07685aa5ddfa41315
James Rossiter <james@jamesrossiter.co.uk>
2015-02-16 14:41:37 +0000

    Ability to print letters using Print button on modal view

--- root/webapp/app/scripts/controllers/letters.js
+++ root/webapp/app/scripts/controllers/letters.js
@@ -6,6 +6,21 @@ function ($scope, $modalInstance, letter) {
     $scope.cancel = function () {
         $modalInstance.dismiss('cancel');
     };
+
+    $scope.print = function() {
+        // ie8 compatibility
+        var printContent = $('.modal-content').clone();
+        printContent.children('.modal-footer').remove();
+        var windowUrl = 'PatientView';
+        var uniqueName = new Date();
+        var windowName = 'Print' + uniqueName.getTime();
+        var printWindow = window.open(windowUrl, windowName, 'left=50000,top=50000,width=0,height=0');
+        printWindow.document.write(printContent.html());
+        printWindow.document.close();
+        printWindow.focus();
+        printWindow.print();
+        printWindow.close();
+    };
 }];
 // delete letter modal instance controller
 var DeleteLetterModalInstanceCtrl = ['$scope', '$modalInstance', 'letter', 'LetterService',
