5f3e0819734fea0e38229a7a7fc5f57404c30717
James Rossiter <james@jamesrossiter.co.uk>
2015-02-18 17:40:17 +0000

    javadoc style comments for API Util.java

--- root/api/src/main/java/org/patientview/api/util/Util.java
+++ root/api/src/main/java/org/patientview/api/util/Util.java
@@ -27,6 +27,8 @@ import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 /**
+ * Static utility methods.
+ *
  * Created by james@solidstategroup.com
  * Created on 05/06/2014
  */
@@ -35,15 +37,12 @@ public final class Util {
     private Util() { }
 
     /**
-     * This is convert the Iterable<T> type passed for Spring DAO interface into
-     * a more useful typed List.
-     *
-     * @param iterable
-     * @param <T>
-     * @return
+     * Convert the Iterable<T> type passed for Spring DAO interface into a more useful typed List.
+     * @param iterable Iterable to convert to typed List
+     * @param <T> Type of List to create
+     * @return Typed List
      */
     public static <T> List<T> convertIterable(Iterable<T> iterable) {
-
         if (iterable == null) {
             return Collections.emptyList();
         }
@@ -55,10 +54,15 @@ public final class Util {
             list.add(lookupIterator.next());
         }
         return list;
-
     }
 
-    // check string appears in enum
+    /**
+     * Check a String appears in an Enum.
+     * @param value String value to find in Enum
+     * @param enumClass Class of Enum to search
+     * @param <E> Type of Enum
+     * @return True if String exists in Enum, false if not
+     */
     public static <E extends Enum<E>> boolean isInEnum(String value, Class<E> enumClass) {
         for (E e : enumClass.getEnumConstants()) {
             if (e.name().equals(value)) { return true; }
@@ -67,6 +71,12 @@ public final class Util {
     }
 
     // Retrieve the list of Roles from the annotation.
+
+    /**
+     * Retrieve a an Array of RoleName from an aspect JoinPoint.
+     * @param joinPoint Aspect JoinPoint
+     * @return Array of RoleName
+     */
     public static RoleName[] getRoles(JoinPoint joinPoint) {
         final org.aspectj.lang.Signature signature = joinPoint.getStaticPart().getSignature();
         if (signature instanceof MethodSignature) {
@@ -85,7 +95,12 @@ public final class Util {
         return null;
     }
 
-    public static RoleName[] getRolesFromAnnotation(Annotation annotation) {
+    /**
+     * Retrieve a an Array of RoleName from an aspect Annotation.
+     * @param annotation Aspect Annotation
+     * @return Array of RoleName
+     */
+    private static RoleName[] getRolesFromAnnotation(Annotation annotation) {
         Method[] methods = annotation.annotationType().getMethods();
         for (Method method : methods) {
             String name = method.getName();
@@ -107,6 +122,12 @@ public final class Util {
                         + annotation.getClass().getCanonicalName());
     }
 
+    /**
+     * Convert a collection of GrantedAuthority to a typed List.
+     * @param grantedAuthorities Collection of GrantedAuthority
+     * @param <T> Type to convert to
+     * @return List of converted GrantedAuthority
+     */
     public static <T> List<T> convertAuthorities(Collection<? extends GrantedAuthority>  grantedAuthorities) {
         List<T> list = new ArrayList<>();
         for (GrantedAuthority grantedAuthority : grantedAuthorities) {
@@ -115,6 +136,12 @@ public final class Util {
         return list;
     }
 
+    /**
+     * Check if current User is a member of a Group with a certain Role.
+     * @param groupId ID of Group to find Role membership
+     * @param roleNames One or more RoleName to find membership of
+     * @return True if User is a member, false if not
+     */
     public static boolean doesContainGroupAndRole(Long groupId, RoleName... roleNames) {
         if (CollectionUtils.isEmpty(getGroupRoles())) {
             return false;
@@ -133,7 +160,12 @@ public final class Util {
         return false;
     }
 
-    // used for specialty admins
+    /**
+     * Check if current User is a member of a Group with a certain Role, including child groups (for specialty admins).
+     * @param groupId ID of Group to find Role membership
+     * @param roleName RoleName to find membership of
+     * @return True if User is a member, false if not
+     */
     public static boolean doesContainChildGroupAndRole(Long groupId, RoleName roleName) {
         if (CollectionUtils.isEmpty(getGroupRoles())) {
             return false;
@@ -155,7 +187,12 @@ public final class Util {
         return false;
     }
 
-    // used for specialty admins
+    /**
+     * Check if current User is a member of a Group with a certain Role, including parent groups (for specialty admins).
+     * @param groupId ID of Group to find Role membership
+     * @param roleName RoleName to find membership of
+     * @return True if User is a member, false if not
+     */
     public static boolean doesContainParentGroupAndRole(Long groupId, RoleName roleName) {
         if (CollectionUtils.isEmpty(getGroupRoles())) {
             return false;
@@ -177,6 +214,12 @@ public final class Util {
         return false;
     }
 
+    /**
+     * Check if current User has one or more Roles.
+     * @param roleNames One or more RoleName to find membership of
+     * @return True if User is a member, false if not
+     * @throws SecurityException
+     */
     public static boolean doesContainRoles(RoleName... roleNames) throws SecurityException {
 
         if (CollectionUtils.isEmpty(getGroupRoles())) {
@@ -193,6 +236,10 @@ public final class Util {
 
     }
 
+    /**
+     * Get GroupRoles from current SecurityContext, originally stored after login.
+     * @return List of GroupRole
+     */
     public static List<GroupRole> getGroupRoles() {
         if (SecurityContextHolder.getContext() == null
                 || SecurityContextHolder.getContext().getAuthentication() == null) {
@@ -203,6 +250,10 @@ public final class Util {
         return groupRoles;
     }
 
+    /**
+     * Get the User object associated with the current security context Principal.
+     * @return User object
+     */
     public static User getUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
@@ -213,6 +264,11 @@ public final class Util {
         return (User) authentication.getPrincipal();
     }
 
+    /**
+     * Create a FHIR ResourceReference from a UUID.
+     * @param uuid UUID to convert to FHIR ResourceReference
+     * @return ResourceReference object
+     */
     public static ResourceReference createFhirResourceReference(UUID uuid) {
         ResourceReference resourceReference = new ResourceReference();
         resourceReference.setDisplaySimple(uuid.toString());
@@ -220,6 +276,13 @@ public final class Util {
         return resourceReference;
     }
 
+    /**
+     * Get the time difference in TimeUnit between two dates, used for timing migration.
+     * @param date1 Start time
+     * @param date2 End time
+     * @param timeUnit TimeUnit e.g. TimeUnit.SECONDS
+     * @return long representation of time difference
+     */
     public static long getDateDiff(Date date1, Date date2, TimeUnit timeUnit) {
         long diffInMilliseconds = date2.getTime() - date1.getTime();
         return timeUnit.convert(diffInMilliseconds, TimeUnit.MILLISECONDS);
