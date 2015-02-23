43f0beb2d462ca1627b6a8d0adcf177c91a8e7e5
James Rossiter <james@jamesrossiter.co.uk>
2015-02-23 10:27:31 +0000

    Card #330: pagination moved to directive, js out of codes, conversations, groups, joinrequestadmin, log, patients, resultstable, staff

--- root/webapp/app/scripts/controllers/conversations.js
+++ root/webapp/app/scripts/controllers/conversations.js
@@ -88,77 +88,7 @@ function ($scope, $modal, $q, $filter, ConversationService, GroupService, UserSe
     $scope.folders = [];
     $scope.folders.push({name:'INBOX', description:'Inbox'}, {name:'ARCHIVED', description:'Archived'});
     $scope.selectedFolder = $scope.folders[0].name;
-
-    // pagination
-    $scope.pageCount = function() {
-        return Math.ceil($scope.total / $scope.itemsPerPage);
-    };
-
-    $scope.range = function() {
-        var rangeSize = $scope.itemsPerPage;
-        var pageNumbers = [];
-        var startPage;
-
-        if (($scope.currentPage - $scope.itemsPerPage / 2) < 0) {
-            startPage = 0;
-        } else {
-            startPage = $scope.currentPage - $scope.itemsPerPage / 2;
-        }
-
-        if (startPage > $scope.pageCount() - rangeSize) {
-            startPage = $scope.pageCount() - rangeSize;
-        }
-
-        for (var i = startPage; i < startPage + rangeSize; i++) {
-            if (i > -1) {
-                pageNumbers.push(i);
-            }
-        }
-
-        return pageNumbers;
-    };
-
-    $scope.setPage = function(pageNumber) {
-        if (pageNumber > -1 && pageNumber < $scope.totalPages) {
-            $scope.currentPage = pageNumber;
-        }
-    };
-
-    $scope.firstPage = function() {
-        $scope.currentPage = 0;
-    };
-
-    $scope.prevPage = function() {
-        if ($scope.currentPage > 0) {
-            $scope.currentPage--;
-        }
-    };
-
-    $scope.nextPage = function() {
-        if ($scope.currentPage < $scope.totalPages - 1) {
-            $scope.currentPage++;
-        }
-    };
-
-    $scope.lastPage = function() {
-        $scope.currentPage = $scope.totalPages - 1;
-    };
-
-    $scope.firstPageDisabled = function() {
-        return (($scope.currentPage - $scope.itemsPerPage / 2) < 0) ? 'hidden' : '';
-    };
-
-    $scope.prevPageDisabled = function() {
-        return $scope.currentPage === 0 ? 'hidden' : '';
-    };
-
-    $scope.nextPageDisabled = function() {
-        return $scope.currentPage === $scope.pageCount() - 1 ? 'hidden' : '';
-    };
-
-    $scope.lastPageDisabled = function() {
-        return ($scope.currentPage + 6 > $scope.pageCount()) ? 'hidden' : '';
-    };
+    
     // get page of data every time currentPage is changed
     $scope.$watch('currentPage', function() {
         getItems();
