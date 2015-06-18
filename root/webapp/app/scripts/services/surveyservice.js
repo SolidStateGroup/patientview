caf5025e39aabee8679f3dbf16a5def2abcd7b8e
James Rossiter <james@jamesrossiter.co.uk>
2015-06-18 11:01:56 +0100

    Card #158: ie8 fixes

--- root/webapp/app/scripts/services/surveyservice.js
+++ root/webapp/app/scripts/services/surveyservice.js
@@ -4,7 +4,6 @@ angular.module('patientviewApp').factory('SurveyService', ['$q', 'Restangular',
     function ($q, Restangular) {
         return {
             getByType: function (type) {
-                console.log(type);
                 var deferred = $q.defer();
                 // GET /surveys/type/{type}
                 Restangular.one('surveys/type', type).get().then(function(successResult) {
