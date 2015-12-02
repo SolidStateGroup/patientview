bdc6b7ca6872d67e741d8b4cd4a57c100ed0bec4
James Rossiter <james@jamesrossiter.co.uk>
2015-12-02 17:53:49 +0000

    Card #408: STAFF_ENTERED in HiddenGroupCodes, more create fhir patient, fhir links etc

--- root/api/src/main/java/org/patientview/api/service/impl/ConditionServiceImpl.java
+++ root/api/src/main/java/org/patientview/api/service/impl/ConditionServiceImpl.java
@@ -4,21 +4,40 @@ import org.apache.commons.lang.StringUtils;
 import org.hl7.fhir.instance.model.CodeableConcept;
 import org.hl7.fhir.instance.model.Condition;
 import org.hl7.fhir.instance.model.DateAndTime;
+import org.hl7.fhir.instance.model.Patient;
+import org.hl7.fhir.instance.model.ResourceReference;
 import org.hl7.fhir.instance.model.ResourceType;
+import org.patientview.api.service.CodeService;
 import org.patientview.api.service.ConditionService;
+import org.patientview.api.service.GroupService;
+import org.patientview.api.service.LookupService;
+import org.patientview.api.service.PatientService;
 import org.patientview.api.service.UserService;
 import org.patientview.api.util.Util;
 import org.patientview.config.exception.ResourceForbiddenException;
 import org.patientview.config.exception.ResourceNotFoundException;
 import org.patientview.config.exception.FhirResourceException;
+import org.patientview.persistence.model.Code;
 import org.patientview.persistence.model.FhirCondition;
+import org.patientview.persistence.model.FhirDatabaseEntity;
 import org.patientview.persistence.model.FhirLink;
+import org.patientview.persistence.model.Group;
+import org.patientview.persistence.model.Identifier;
+import org.patientview.persistence.model.Lookup;
 import org.patientview.persistence.model.User;
+import org.patientview.persistence.model.enums.CodeTypes;
+import org.patientview.persistence.model.enums.HiddenGroupCodes;
+import org.patientview.persistence.model.enums.LookupTypes;
+import org.patientview.persistence.repository.FhirLinkRepository;
+import org.patientview.persistence.repository.UserRepository;
 import org.patientview.persistence.resource.FhirResource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import javax.inject.Inject;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.UUID;
 
@@ -30,7 +49,25 @@ import java.util.UUID;
 public class ConditionServiceImpl extends AbstractServiceImpl<ConditionServiceImpl> implements ConditionService {
 
     @Inject
+    private CodeService codeService;
+
+    @Inject
+    private FhirLinkRepository fhirLinkRepository;
+
+    @Inject
     private FhirResource fhirResource;
+
+    @Inject
+    private GroupService groupService;
+
+    @Inject
+    private LookupService lookupService;
+
+    @Inject
+    private PatientService patientService;
+
+    @Inject
+    private UserRepository userRepository;
 
     @Inject
     private UserService userService;
@@ -87,11 +124,67 @@ public class ConditionServiceImpl extends AbstractServiceImpl<ConditionServiceIm
     public void staffAddCondition(Long patientUserId, String code)
             throws ResourceForbiddenException, ResourceNotFoundException, FhirResourceException {
 
-        User patient = userService.get(patientUserId);
-        if (!userService.currentUserCanGetUser(patient)) {
+        User patientUser = userService.get(patientUserId);
+        if (!userService.currentUserCanGetUser(patientUser)) {
             throw new ResourceForbiddenException("Forbidden");
         }
 
+        if (CollectionUtils.isEmpty(patientUser.getIdentifiers())) {
+            throw new ResourceNotFoundException("Patient must have at least one Identifier (NHS Number or other)");
+        }
+
+        // sort identifiers and choose first (must have fhir link with identifier to link to fhir database
+        List<Identifier> identifiersSorted = new ArrayList<>(patientUser.getIdentifiers());
+        Collections.sort(identifiersSorted);
+        Identifier patientIdentifier = identifiersSorted.get(0);
+
+        Group staffEnteredGroup = groupService.findByCode(HiddenGroupCodes.STAFF_ENTERED.toString());
+        if (staffEnteredGroup == null) {
+            throw new ResourceNotFoundException("Group for staff entered data does not exist");
+        }
+
+        Lookup codeLookup = lookupService.findByTypeAndValue(LookupTypes.CODE_TYPE, CodeTypes.DIAGNOSIS.toString());
+        List<Code> codes = codeService.findAllByCodeAndType(code, codeLookup);
+
+        if (CollectionUtils.isEmpty(codes)) {
+            throw new ResourceNotFoundException("Cannot find code");
+        }
+
         User staff = getCurrentUser();
+
+        List<FhirLink> fhirLinks = fhirLinkRepository.findByUserAndGroupAndIdentifier(
+                patientUser, staffEnteredGroup, identifiersSorted.get(0));
+
+        FhirLink fhirLink;
+
+        if (CollectionUtils.isEmpty(fhirLinks)) {
+            // create FHIR Patient & fhirlink if not exists with STAFF_ENTERED group, userId and identifier
+            Patient patient = patientService.buildPatient(patientUser, patientIdentifier);
+            FhirDatabaseEntity fhirPatient
+                    = fhirResource.createEntity(patient, ResourceType.Patient.name(), "patient");
+
+            // create FhirLink to link user to FHIR Patient at group PATIENT_ENTERED
+            fhirLink = new FhirLink();
+            fhirLink.setUser(patientUser);
+            fhirLink.setIdentifier(patientIdentifier);
+            fhirLink.setGroup(staffEnteredGroup);
+            fhirLink.setResourceId(fhirPatient.getLogicalId());
+            fhirLink.setVersionId(fhirPatient.getVersionId());
+            fhirLink.setResourceType(ResourceType.Patient.name());
+            fhirLink.setActive(true);
+
+            if (CollectionUtils.isEmpty(patientUser.getFhirLinks())) {
+                patientUser.setFhirLinks(new HashSet<FhirLink>());
+            }
+
+            patientUser.getFhirLinks().add(fhirLink);
+            userRepository.save(patientUser);
+        } else {
+            fhirLink = fhirLinks.get(0);
+        }
+
+        // fhir link now exists
+        ResourceReference patientReference = Util.createFhirResourceReference(fhirLink.getResourceId());
+
     }
 }
--- root/api/src/test/java/org/patientview/api/service/ConditionServiceTest.java
+++ root/api/src/test/java/org/patientview/api/service/ConditionServiceTest.java
@@ -7,14 +7,25 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.patientview.api.service.impl.ConditionServiceImpl;
+import org.patientview.persistence.model.Code;
+import org.patientview.persistence.model.FhirLink;
 import org.patientview.persistence.model.Group;
 import org.patientview.persistence.model.GroupRole;
+import org.patientview.persistence.model.Identifier;
+import org.patientview.persistence.model.Lookup;
 import org.patientview.persistence.model.Role;
 import org.patientview.persistence.model.User;
+import org.patientview.persistence.model.enums.CodeTypes;
+import org.patientview.persistence.model.enums.HiddenGroupCodes;
+import org.patientview.persistence.model.enums.IdentifierTypes;
+import org.patientview.persistence.model.enums.LookupTypes;
 import org.patientview.persistence.model.enums.RoleName;
+import org.patientview.persistence.repository.FhirLinkRepository;
 import org.patientview.test.util.TestUtils;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import static org.mockito.Matchers.eq;
@@ -25,6 +36,18 @@ import static org.mockito.Mockito.when;
  * Created on 02/12/2015
  */
 public class ConditionServiceTest {
+
+    @Mock
+    CodeService codeService;
+
+    @Mock
+    FhirLinkRepository fhirLinkRepository;
+
+    @Mock
+    GroupService groupService;
+
+    @Mock
+    LookupService lookupService;
 
     @Mock
     UserService userService;
@@ -44,6 +67,8 @@ public class ConditionServiceTest {
 
     @Test
     public void testStaffAddCondition() throws Exception {
+        String code = "00";
+
         // user and security
         Group group = TestUtils.createGroup("testGroup");
         Role role = TestUtils.createRole(RoleName.UNIT_ADMIN);
@@ -62,6 +87,36 @@ public class ConditionServiceTest {
         groupRolesPatient.add(groupRolePatient);
         patient.setGroupRoles(groupRolesPatient);
 
+        // patient identifier
+        Identifier identifier = TestUtils.createIdentifier(
+                TestUtils.createLookup(TestUtils.createLookupType(LookupTypes.IDENTIFIER),
+                        IdentifierTypes.NHS_NUMBER.toString()), patient, "1111111111");
+        List<Identifier> identifiers = new ArrayList<>();
+        identifiers.add(identifier);
+
+        // staff entered results group
+        Group staffEntered = TestUtils.createGroup("staffEntered");
+        staffEntered.setCode(HiddenGroupCodes.STAFF_ENTERED.toString());
+
+        // code
+        Lookup lookup = TestUtils.createLookup(
+                TestUtils.createLookupType(LookupTypes.CODE_TYPE), CodeTypes.DIAGNOSIS.toString());
+        Code codeEntity = TestUtils.createCode("00");
+        List<Code> codes = new ArrayList<>();
+        codes.add(codeEntity);
+
+        // fhir link
+        FhirLink fhirLink = TestUtils.createFhirLink(patient, identifier);
+        fhirLink.setGroup(staffEntered);
+        List<FhirLink> fhirLinks = new ArrayList<>();
+        fhirLinks.add(fhirLink);
+
+        when(codeService.findAllByCodeAndType(eq(code), eq(lookup))).thenReturn(codes);
+        when(fhirLinkRepository.findByUserAndGroupAndIdentifier(eq(patient), eq(staffEntered), eq(identifier)))
+                .thenReturn(fhirLinks);
+        when(groupService.findByCode(eq(HiddenGroupCodes.STAFF_ENTERED.toString()))).thenReturn(staffEntered);
+        when(lookupService.findByTypeAndValue(eq(LookupTypes.CODE_TYPE), eq(CodeTypes.DIAGNOSIS.toString())))
+                .thenReturn(lookup);
         when(userService.get(eq(patient.getId()))).thenReturn(patient);
         when(userService.currentUserCanGetUser(eq(patient))).thenReturn(true);
 
