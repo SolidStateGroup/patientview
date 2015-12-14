d64b53aadbcce28f280c86b96fb42a569cfea8a3
James Rossiter <james@jamesrossiter.co.uk>
2015-12-14 11:53:34 +0000

    Card #428: controller test

---
+++ root/api/src/test/java/org/patientview/api/controller/ExportControllerTest.java
@@ -0,0 +1,68 @@
+package org.patientview.api.controller;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.patientview.api.service.ExportService;
+import org.patientview.persistence.model.Group;
+import org.patientview.persistence.model.GroupRole;
+import org.patientview.persistence.model.Role;
+import org.patientview.persistence.model.User;
+import org.patientview.persistence.model.enums.RoleName;
+import org.patientview.persistence.model.enums.SurveyTypes;
+import org.patientview.test.util.TestUtils;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by jamesr@solidstategroup.com
+ * Created on 14/12/2015
+ */
+public class ExportControllerTest {
+
+    @Mock
+    private ExportService exportService;
+
+    @InjectMocks
+    private ExportController exportController;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(exportController).build();
+    }
+
+    @After
+    public void tearDown() {
+        TestUtils.removeAuthentication();
+    }
+
+    @Test
+    public void testDownloadSurveyResponses() throws Exception {
+
+        // user and security
+        Group group = TestUtils.createGroup("testGroup");
+        Role role = TestUtils.createRole(RoleName.PATIENT);
+        User user = TestUtils.createUser("testUser");
+        GroupRole groupRole = TestUtils.createGroupRole(role, group, user);
+        Set<GroupRole> groupRoles = new HashSet<>();
+        groupRoles.add(groupRole);
+        TestUtils.authenticateTest(user, groupRoles);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/user/" + user.getId() + "/export/survey/"
+                + SurveyTypes.IBD_CONTROL + "/download"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+}
+
+
