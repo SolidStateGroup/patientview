b511cab1c4e9948fac6ad4b93b9b069a76db06d6
James Rossiter <james@jamesrossiter.co.uk>
2016-01-12 17:32:44 +0000

    Card #435: support for building SurveyResponse object and saving

--- root/webapp/app/scripts/controllers/ibdselfmanagement.js
+++ root/webapp/app/scripts/controllers/ibdselfmanagement.js
@@ -1,7 +1,8 @@
 'use strict';
 
-angular.module('patientviewApp').controller('IbdSelfManagementCtrl', ['$scope', '$rootScope', 'SurveyResponseService',
-function ($scope, $rootScope, SurveyResponseService) {
+angular.module('patientviewApp').controller('IbdSelfManagementCtrl', ['$scope', '$rootScope', 'SurveyService',
+    'SurveyResponseService',
+function ($scope, $rootScope, SurveyService, SurveyResponseService) {
 
     $scope.cancel = function() {
         if (confirm("This will discard any changes you've made. Do you wish to continue?")) {
@@ -9,9 +10,49 @@ function ($scope, $rootScope, SurveyResponseService) {
         }
     };
 
-    $scope.init = function() {
+    var getSurvey = function() {
+        $scope.loadingMessage = 'Loading Self-Management Programme';
         $scope.loading = true;
+        var i, j;
+
+        SurveyService.getByType($scope.surveyType).then(function(survey) {
+            $scope.survey = survey;
+            $scope.questions = [];
+
+            // create map of question id to question type, used when creating object to send to backend
+            for (i = 0; i < survey.questionGroups.length; i++) {
+                for (j = 0; j < survey.questionGroups[i].questions.length; j++) {
+                    $scope.questions.push(survey.questionGroups[i].questions[j]);
+                }
+            }
+        }, function () {
+            alert('error getting self-management programme details')
+        });
+    };
+
+    var getSurveyResponses = function() {
+        $scope.loadingMessage = 'Loading Self-Management Programme';
+        $scope.loading = true;
+        SurveyResponseService.getByUserAndSurveyType($scope.loggedInUser.id, $scope.surveyType)
+        .then(function(surveyResponses) {
+            if (surveyResponses.length) {
+                surveyResponses = _.sortBy(surveyResponses, 'date').reverse();
+                var response = surveyResponses[0];
+                // build survey response object based on hardcoded survey responses
+            } else {
+                // no existing survey responses, set defaults
+                $scope.selfManagement.IBD_SELF_MANAGEMENT_PLAN_OF_CARE = '1. Ensure that you are taking your medications regularly \n2. Ensure that you are taking the correct dose of medication, if you are not sure you should check with the IBD Nurse team \n3. \n4.';
+            }
+            $scope.loading = false;
+        }, function() {
+            alert('Error retrieving self management programme');
+            $scope.loading = false;
+        });
+    };
+
+    $scope.init = function() {
         $scope.selfManagement = {};
+        delete $scope.successMessage;
 
         // check if viewing as patient
         $scope.isStaff = $rootScope.previousLoggedInUser ? true : false;
@@ -25,30 +66,48 @@ function ($scope, $rootScope, SurveyResponseService) {
         }
 
         $scope.surveyType = 'IBD_SELF_MANAGEMENT';
+        getSurvey();
         getSurveyResponses();
     };
 
-    var getSurveyResponses = function() {
+    $scope.save = function() {
+        // create survey responses based on ui model (similar to surveyresponses.js)
+        delete $scope.successMessage;
+        $scope.loadingMessage = 'Saving Self-Management Programme';
         $scope.loading = true;
-        SurveyResponseService.getByUserAndSurveyType($scope.loggedInUser.id, $scope.surveyType)
-        .then(function(surveyResponses) {
-            if (surveyResponses.length) {
-                surveyResponses = _.sortBy(surveyResponses, 'date').reverse();
+        var i;
 
-                // build survey response object based on hardcoded survey responses
-            } else {
-                // no existing survey responses, set defaults
-                $scope.selfManagement.planOfCare = '1. Ensure that you are taking your medications regularly \n2. Ensure that you are taking the correct dose of medication, if you are not sure you should check with the IBD Nurse team \n3. \n4.';
+        // build object to send to back end
+        var surveyResponse = {};
+        surveyResponse.user = {};
+        surveyResponse.user.id = $scope.loggedInUser.id;
+        //surveyResponse.staffUser = {};
+        //surveyResponse.staffUser.id = $rootScope.previousLoggedInUser.id;
+        surveyResponse.survey = {};
+        surveyResponse.survey.id = $scope.survey.id;
+        surveyResponse.questionAnswers = [];
+        surveyResponse.date = new Date();
+
+        for (i = 0; i < $scope.questions.length; i++) {
+            var type = $scope.questions[i].type;
+
+            if ($scope.selfManagement[type] != null && $scope.selfManagement[type] != undefined
+                && $scope.selfManagement[type].length) {
+                var questionAnswer = {};
+                questionAnswer.value = $scope.selfManagement[type];
+                questionAnswer.question = {};
+                questionAnswer.question.id = $scope.questions[i].id;
+                surveyResponse.questionAnswers.push(questionAnswer);
             }
-            $scope.loading = false;
-        }, function() {
-            alert('Error retrieving self management programme');
-            $scope.loading = false;
-        });
-    };
+        }
 
-    var save = function() {
-        // create survey responses based on ui model
+        SurveyResponseService.add(surveyResponse.user.id, surveyResponse).then(function () {
+            $scope.successMessage = 'Saved updated self-management programme';
+            getSurveyResponses();
+        }, function () {
+            $scope.loading = false;
+            alert('There was an error saving');
+        });
     };
 
     $scope.init();
