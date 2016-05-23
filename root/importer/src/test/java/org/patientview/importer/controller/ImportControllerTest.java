15479990405097169fae6ae441b2e629a3dd34b3
James Rossiter <james@jamesrossiter.co.uk>
2016-05-23 14:51:10 +0100

    Card #334: fix for highcharts rendering over dropdown

--- root/importer/src/test/java/org/patientview/importer/controller/ImportControllerTest.java
+++ root/importer/src/test/java/org/patientview/importer/controller/ImportControllerTest.java
@@ -92,8 +92,8 @@ public class ImportControllerTest {
         String content = new String(Files.readAllBytes(Paths.get(file.getPath())));
 
         org.apache.http.client.HttpClient httpClient = new DefaultHttpClient();
-        //String postUrl="http://localhost:8081/importer/import/survey";
-        String postUrl="https://test.patientview.org/importer/import/survey";
+        String postUrl="http://localhost:8081/importer/import/survey";
+        //String postUrl="https://test.patientview.org/importer/import/survey";
 
         HttpPost post = new HttpPost(postUrl);
         StringEntity postingString = new StringEntity(content);
@@ -106,8 +106,8 @@ public class ImportControllerTest {
     @Test
     @Ignore
     public void testSurveyResponseImport() throws Exception {
-        //String fileName = "data/xml/survey_response/survey_response_PROMS_1.xml";
-        String fileName = "data/xml/survey_response/survey_response_PROMS_2_missing.xml";
+        String fileName = "data/xml/survey_response/survey_response_PROMS_1.xml";
+        //String fileName = "data/xml/survey_response/survey_response_PROMS_2_missing.xml";
         //String fileName = "data/xml/survey_response/survey_response_EQ5D_1.xml";
         //String fileName = "data/xml/survey_response/survey_response_EQ5D_2_missing.xml";
 
@@ -116,8 +116,8 @@ public class ImportControllerTest {
         String content = new String(Files.readAllBytes(Paths.get(file.getPath())));
 
         org.apache.http.client.HttpClient httpClient = new DefaultHttpClient();
-        //String postUrl="http://localhost:8081/importer/import/surveyresponse";
-        String postUrl="https://test.patientview.org/importer/import/surveyresponse";
+        String postUrl="http://localhost:8081/importer/import/surveyresponse";
+        //String postUrl="https://test.patientview.org/importer/import/surveyresponse";
 
         HttpPost post = new HttpPost(postUrl);
         StringEntity postingString = new StringEntity(content);
