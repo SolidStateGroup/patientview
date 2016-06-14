be271d03e78635fee45d6be06a197573b90574ff
James Rossiter <james@jamesrossiter.co.uk>
2016-06-14 13:13:13 +0100

    Card #264: enter diagnoses button in my conditions

--- root/webapp/app/scripts/controllers/enterconditions.js
+++ root/webapp/app/scripts/controllers/enterconditions.js
@@ -1,11 +1,35 @@
 'use strict';
-angular.module('patientviewApp').controller('MyConditionsEnterConditionsCtrl',['$scope', 'PatientService', 'UserService', '$modal', 'GpService', '$rootScope',
-function ($scope, PatientService, UserService, $modal, GpService, $rootScope) {
+angular.module('patientviewApp').controller('MyConditionsEnterConditionsCtrl',['$scope', 'CodeService', 'DiagnosisService', '$modal', 'StaticDataService',
+function ($scope, CodeService, DiagnosisService, $modal, StaticDataService) {
 
     $scope.init = function(){
 
     };
 
+    $scope.showEnterDiagnosesModal = function () {
+        var modalInstance = $modal.open({
+            templateUrl: 'views/modal/enterDiagnosesModal.html',
+            controller: EnterDiagnosesModalInstanceCtrl,
+            size: 'lg',
+            resolve: {
+                CodeService: function () {
+                    return CodeService;
+                },
+                DiagnosisService: function () {
+                    return DiagnosisService;
+                },
+                StaticDataService: function () {
+                    return StaticDataService;
+                }
+            }
+        });
+
+        modalInstance.result.then(function () {
+            // ok (not used)
+        }, function () {
+            // closed
+        });
+    };
 
     $scope.init();
 }]);
