3691a8516e12d847a64c081cfbc3ceb1e57f3d3a
Pavlo Maks <pavlo@solidstategroup.com>
2016-08-16 17:14:31 +0100

    Added new enumerator LinkTypes

---
+++ root/common/src/main/java/org/patientview/persistence/model/enums/LinkTypes.java
@@ -0,0 +1,10 @@
+package org.patientview.persistence.model.enums;
+
+/**
+ * Types of Links mapped to what stored in database
+ */
+public enum LinkTypes {
+    NHS_CHOICES,
+    MEDLINE_PLUS,
+    CUSTOM
+}
