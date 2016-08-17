efa52f3fbe1082f2cddaa9f5e2aad23871a61be1
Pavlo Maks <pavlo@solidstategroup.com>
2016-08-17 11:00:52 +0100

    Added ids to LinkTypes mapping

--- root/common/src/main/java/org/patientview/persistence/model/enums/LinkTypes.java
+++ root/common/src/main/java/org/patientview/persistence/model/enums/LinkTypes.java
@@ -4,7 +4,24 @@ package org.patientview.persistence.model.enums;
  * Types of Links mapped to what stored in database
  */
 public enum LinkTypes {
-    NHS_CHOICES,
-    MEDLINE_PLUS,
-    CUSTOM
+    NHS_CHOICES{
+        @Override
+        public long id() {
+            return 135;
+        }
+    },
+    MEDLINE_PLUS{
+        @Override
+        public long id() {
+            return 136;
+        }
+    },
+    CUSTOM{
+        @Override
+        public long id() {
+            return 137;
+        }
+    };
+
+    public abstract long id();
 }
