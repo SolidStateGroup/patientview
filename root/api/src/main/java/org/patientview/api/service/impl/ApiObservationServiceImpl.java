bf59f0c4932e1ad6bb26a39070927c4d3f8a7d96
Pavlo Maks <pavlo@solidstategroup.com>
2016-10-24 13:15:56 +0100

    Added log info for result changes

--- root/api/src/main/java/org/patientview/api/service/impl/ApiObservationServiceImpl.java
+++ root/api/src/main/java/org/patientview/api/service/impl/ApiObservationServiceImpl.java
@@ -376,7 +376,8 @@ public class ApiObservationServiceImpl extends AbstractServiceImpl<ApiObservatio
         information.append("', '");
         information.append(enteredResult.getValue());
         information.append("']");
-
+        LOG.info("Result updated: " + information.toString());
+        
         // Record audit action
         Audit audit = new Audit();
         audit.setAuditActions(AuditActions.PATIENT_ENTERED_RESULT_EDITED);
