983a922f0d496e539b094161ae6b41b9502bf3d8
Pavlo Maks <pavlo@solidstategroup.com>
2017-01-10 14:38:43 +0000

    Implemented Note entity model

---
+++ root/common/src/main/java/org/patientview/persistence/model/Note.java
@@ -0,0 +1,57 @@
+package org.patientview.persistence.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.patientview.persistence.model.enums.NoteTypes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+/**
+ * Notes entity model
+ */
+@Entity
+@Table(name = "pv_note")
+public class Note extends AuditModel {
+
+    @Column(name = "note_type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private NoteTypes noteType;
+
+    @JsonIgnore
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "body")
+    private String body;
+
+    public NoteTypes getNoteType() {
+        return noteType;
+    }
+
+    public void setNoteType(NoteTypes noteType) {
+        this.noteType = noteType;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
