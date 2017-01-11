c5f36884dc82922a068ffb01add9b56705eb0522
Pavlo Maksymchuk <pavlo.maksymchuk@yclip.co.uk>
2017-01-11 10:28:50 +0000

    Implemented note repository

---
+++ root/data/src/main/java/org/patientview/persistence/repository/NoteRepository.java
@@ -0,0 +1,22 @@
+package org.patientview.persistence.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Note JPA repository
+ */
+@Repository
+@Transactional(propagation = Propagation.MANDATORY)
+public interface NoteRepository extends CrudRepository<Note, Long> {
+
+    List<Note> findByUser(@Param("user") User user);
+
+    @Query("SELECT   n " +
+            "FROM    Note n " +
+            "JOIN    n.user u " +
+            "WHERE   u = :user AND noteType:noteType")
+    List<Note> findByUserAndNoteType(@Param("user") User user, NoteTypes noteType);
+}
