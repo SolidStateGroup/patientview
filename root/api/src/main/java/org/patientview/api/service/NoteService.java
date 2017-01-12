1f1e4dc899ef581b61ad05b5f7f45279ca62e6e6
Pavlo Maks <pavlo@solidstategroup.com>
2017-01-12 17:19:10 +0000

    Note service interface defined

---
+++ root/api/src/main/java/org/patientview/api/service/NoteService.java
@@ -0,0 +1,70 @@
+package org.patientview.api.service;
+
+import org.patientview.api.model.Note;
+import org.patientview.config.exception.ResourceForbiddenException;
+import org.patientview.config.exception.ResourceNotFoundException;
+import org.patientview.persistence.model.enums.NoteTypes;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * NoteService service covers CRUD operation for Notes.
+ */
+@Transactional(propagation = Propagation.REQUIRES_NEW)
+public interface NoteService {
+
+    /**
+     * Add an Note for a User.
+     *
+     * @param userId ID of User adding Note
+     * @param Note   a Note object to be created
+     * @throws ResourceNotFoundException
+     * @throws ResourceForbiddenException
+     */
+    void createNote(Long userId, Note Note)
+            throws ResourceNotFoundException, ResourceForbiddenException;
+
+    /**
+     * Get a Note for a user.
+     *
+     * @param userId ID of User to retrieve
+     * @param noteId an id of the Note to retrieve
+     * @return a Note object
+     * @throws ResourceNotFoundException
+     * @throws ResourceForbiddenException
+     */
+    Note getNote(Long userId, Long noteId) throws ResourceNotFoundException, ResourceForbiddenException;
+
+    /**
+     * Update Note for a user.
+     *
+     * @param userId ID of User to update the Note  for
+     * @param note   a Note containing updated properties
+     * @throws ResourceNotFoundException
+     * @throws ResourceForbiddenException
+     */
+    void updateNote(Long userId, Note note) throws ResourceNotFoundException, ResourceForbiddenException;
+
+
+    /**
+     * Remove a Note for a User.
+     *
+     * @param userId ID of User to remove Note from
+     * @param noteId an ID of the Note to remove
+     * @throws ResourceNotFoundException
+     * @throws ResourceForbiddenException
+     */
+    void removeNote(Long userId, Long noteId) throws ResourceNotFoundException, ResourceForbiddenException;
+
+    /**
+     * Get a list of Notes for a user.
+     *
+     * @param userId   ID of User to retrieve Note for
+     * @param noteType Type of the Note, currently only AlertTypes.DONORVIEW exist
+     * @return List of user's Notes
+     * @throws ResourceNotFoundException
+     */
+    List<Note> getNotes(Long userId, NoteTypes noteType) throws ResourceNotFoundException;
+}
