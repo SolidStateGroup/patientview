0d0b495d2aaa93118d2b1bbed2b0a79c4504ce5c
Pavlo Maks <pavlo@solidstategroup.com>
2017-01-13 12:32:53 +0000

    id setters

--- root/api/src/main/java/org/patientview/api/model/DonorStageData.java
+++ root/api/src/main/java/org/patientview/api/model/DonorStageData.java
@@ -38,6 +38,14 @@ public class DonorStageData implements Serializable {
         }
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public Boolean getBloods() {
         return bloods;
     }
