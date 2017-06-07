f2fc9460ffc0d8ff3d676fadc77740da98ea164b
Pavlo Maks <pavlo@solidstategroup.com>
2017-06-07 15:34:17 +0100

    fixed bug, when getting observation headings by list of codes

--- root/data/src/main/java/org/patientview/persistence/repository/ObservationHeadingRepository.java
+++ root/data/src/main/java/org/patientview/persistence/repository/ObservationHeadingRepository.java
@@ -28,7 +28,7 @@ public interface ObservationHeadingRepository extends CrudRepository<Observation
             "oh.infoLink as infoLink) FROM ObservationHeading oh")
     Page<ObservationHeading> findAllMinimal(Pageable pageable);
 
-    @Query("SELECT oh FROM ObservationHeading oh WHERE lower(oh.code) IN :codes")
+    @Query("SELECT oh FROM ObservationHeading oh WHERE lower(oh.code) IN lower(:codes")
     List<ObservationHeading> findAllByCode(@Param("codes") List<String> codes);
 
     @Query("SELECT oh FROM ObservationHeading oh WHERE lower(oh.code) = lower(:code)")
