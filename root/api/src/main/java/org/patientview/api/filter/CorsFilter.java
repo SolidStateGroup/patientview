3fc0fb18879e586de3198f581a263a6ff62fc222
Pavlo Maks <pavlo@solidstategroup.com>
2017-07-18 14:52:34 +0100

    Added PUT to allowed methods on CorsFilter

--- root/api/src/main/java/org/patientview/api/filter/CorsFilter.java
+++ root/api/src/main/java/org/patientview/api/filter/CorsFilter.java
@@ -42,7 +42,7 @@ public class CorsFilter implements Filter {
         LOG.debug("Adding CORS headers");
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, "
                 + "X-Requested-With, X-Auth-Token");
