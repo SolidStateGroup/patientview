04271cc1521649b2f5208ef571fdeaf22b2cc864
jamesrossiter <jamesr@solidstategroup.com>
2017-08-02 19:23:50 +0100

    hotfix caching: added caching for various service methods

--- root/api/src/main/java/org/patientview/api/service/impl/StaticDataManagerImpl.java
+++ root/api/src/main/java/org/patientview/api/service/impl/StaticDataManagerImpl.java
@@ -9,6 +9,7 @@ import org.patientview.persistence.repository.FeatureRepository;
 import org.patientview.persistence.repository.LookupRepository;
 import org.patientview.persistence.repository.LookupTypeRepository;
 import org.patientview.util.Util;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,22 +28,29 @@ import java.util.List;
 @Transactional(propagation = Propagation.REQUIRES_NEW)
 public class StaticDataManagerImpl extends AbstractServiceImpl<StaticDataManagerImpl>
         implements StaticDataManager {
-
     @Inject
     private LookupRepository lookupRepository;
+
     @Inject
     private LookupTypeRepository lookupTypeRepository;
+
     @Inject
     private FeatureRepository featureRepository;
 
+    @Override
+    @Cacheable(value = "getAllLookups")
     public List<Lookup> getAllLookups() {
         return Util.convertIterable(lookupRepository.findAll());
     }
 
+    @Override
+    @Cacheable(value = "getAllFeatures")
     public List<Feature> getAllFeatures() {
         return Util.convertIterable(featureRepository.findAll());
     }
 
+    @Override
+    @Cacheable(value = "getLookupsByType")
     public List<Lookup> getLookupsByType(LookupTypes type) {
         LookupType lookupType = lookupTypeRepository.findByType(type);
         if (lookupType != null) {
@@ -51,10 +59,14 @@ public class StaticDataManagerImpl extends AbstractServiceImpl<StaticDataManager
         return Collections.emptyList();
     }
 
+    @Override
+    @Cacheable(value = "getLookupByTypeAndValue")
     public Lookup getLookupByTypeAndValue(LookupTypes type, String value) {
         return lookupRepository.findByTypeAndValue(type, value);
     }
 
+    @Override
+    @Cacheable(value = "getFeaturesByType")
     public List<Feature> getFeaturesByType(String featureType) {
         Lookup lookup = lookupRepository.findByTypeAndValue(LookupTypes.FEATURE_TYPE, featureType);
         if (lookup != null) {
