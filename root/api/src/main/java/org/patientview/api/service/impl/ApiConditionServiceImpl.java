2a45c5caf5433b27134fc902b5b1c7d59b22e5ab
Tom Stuart <tom@solidstategroup.com>
2018-03-19 18:27:29 +0000

    Remove logging

--- root/api/src/main/java/org/patientview/api/service/impl/ApiConditionServiceImpl.java
+++ root/api/src/main/java/org/patientview/api/service/impl/ApiConditionServiceImpl.java
@@ -290,7 +290,6 @@ public class ApiConditionServiceImpl extends AbstractServiceImpl<ApiConditionSer
 
             for (Condition condition : conditions) {
                 FhirCondition fhirCondition = createFhirCondition(condition);
-                LOG.info("Temp log  user entered code - " + fhirCondition.getCode());
 
                 // get code and links if present
                 Code code = codeRepository.findOneByCode(fhirCondition.getCode());
