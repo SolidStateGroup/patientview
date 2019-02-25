595bed4ad8ae9887ebf58c3358cfa2178c1e2735
Alex Pask <alex.pask@solidstategroup.com>
2019-02-25 16:58:19 +0000

    feature/opt-pro | Added UT for repository

--- root/data/src/main/java/org/patientview/persistence/repository/SurveySendingFacilityRepository.java
+++ root/data/src/main/java/org/patientview/persistence/repository/SurveySendingFacilityRepository.java
@@ -2,10 +2,11 @@ package org.patientview.persistence.repository;
 
 import org.patientview.persistence.model.SurveySendingFacility;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SurveySendingFacilityRepository extends CrudRepository<SurveySendingFacility, Long> {
 
-    SurveySendingFacility findBySurveyGroup_Id(Long surveyGroupId);
+    SurveySendingFacility findBySurveyGroup_Id(@Param("surveyGroupId") Long surveyGroupId);
 }
