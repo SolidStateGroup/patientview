ec6f272728d3b5fd6135c672b5a367e0bd592d31
Pavlo Maks <pavlo@solidstategroup.com>
2019-10-10 14:12:20 +0100

    new feature type enum

--- root/common/src/main/java/org/patientview/persistence/model/enums/FeatureType.java
+++ root/common/src/main/java/org/patientview/persistence/model/enums/FeatureType.java
@@ -18,7 +18,8 @@ public enum FeatureType {
     RENAL_SURVEY_FEEDBACK_RECIPIENT("Renal Survey Feedback Recipient"),
     RENAL_HEALTH_SURVEYS("Renal Survey Feedback Recipient"),
     ENTER_OWN_DIAGNOSES("Enter Own Diagnoses"),
-    OPT_EPRO("OPT_EPRO");
+    OPT_EPRO("OPT_EPRO"),
+    INS_DIARY("INS Diary Recording");
 
     private String name;
     FeatureType(String name) { this.name = name; }
