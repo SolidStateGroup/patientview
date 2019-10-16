b3c5e1f5d52d7ee4907525825befec443261f9cd
Pavlo Maks <pavlo@solidstategroup.com>
2019-10-16 13:57:53 +0100

    ins diary table sql

--- root/database/src/main/resources/db/migration/V9__INS_Self_Diary.sql
+++ root/database/src/main/resources/db/migration/V9__INS_Self_Diary.sql
@@ -2,3 +2,14 @@ INSERT INTO pv_feature (id, feature_name, description, start_date, creation_date
 VALUES (16, 'INS_DIARY', 'INS Diary Recording', now(), now(), 1);
 
 INSERT INTO pv_feature_feature_type (id, feature_id, type_id) values (22, 16, 14);
+
+CREATE TABLE pv_ins_diary
+(
+  id              BIGINT NOT NULL,
+  user_id         BIGINT NOT NULL REFERENCES pv_user (id),
+  created_by                  BIGINT       REFERENCES pv_user (id) NOT NULL,
+  creation_date               TIMESTAMP    NOT NULL,
+  last_update_date            TIMESTAMP,
+  last_updated_by             BIGINT REFERENCES pv_user (Id),
+  PRIMARY KEY (Id)
+);
