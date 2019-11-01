1ef695c688a37b13aca24f8de778e5fce56f7d06
Pavlo Maks <pavlo@solidstategroup.com>
2019-11-01 16:54:30 +0000

    implemented delete by relapse

--- root/data/src/main/java/org/patientview/persistence/repository/RelapseMedicationRepository.java
+++ root/data/src/main/java/org/patientview/persistence/repository/RelapseMedicationRepository.java
@@ -1,7 +1,10 @@
 package org.patientview.persistence.repository;
 
 import org.patientview.persistence.model.RelapseMedication;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,4 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(propagation = Propagation.MANDATORY)
 public interface RelapseMedicationRepository extends CrudRepository<RelapseMedication, Long> {
 
+    @Modifying(clearAutomatically = true) // note: clearAutomatically required to flush changes straight away
+    @Query("DELETE FROM RelapseMedication WHERE relapse.id = :relapseId")
+    void deleteByRelapse(@Param("relapseId") Long relapseId);
 }
