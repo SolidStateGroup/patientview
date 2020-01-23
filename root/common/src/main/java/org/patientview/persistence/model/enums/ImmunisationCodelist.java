46724d4457dee190de075fd60b8a165a83b6fcaf
Pavlo Maks <pavlo@solidstategroup.com>
2020-01-23 17:26:39 +0000

    mapping immunisation codes

--- root/common/src/main/java/org/patientview/persistence/model/enums/ImmunisationCodelist.java
+++ root/common/src/main/java/org/patientview/persistence/model/enums/ImmunisationCodelist.java
@@ -4,19 +4,40 @@ package org.patientview.persistence.model.enums;
  * Enum Codelist types for Immunisation records
  */
 public enum ImmunisationCodelist {
-    OTHER("Other"),
-    MMR("MMR"),
-    PNEUMOCCAL("Pneumoccal"),
-    ROTAVIRUS("Rotavirus"),
-    MEN_B("MenB"),
-    MEN_ASWY("MenACWY"),
-    VERICELLA("Varicella"),
-    HIB_MENC("Hib/MenC"),
-    FLU("Flu"),
-    HPV("HPV");
+    OTHER("Other", "NA", "NA"),
+    MMR("MMR", "38598009", "Measles-mumps-rubella vaccination"),
+    PNEUMOCCAL("Pneumoccal", "12866006", "Pneumococcal vaccination"),
+    ROTAVIRUS("Rotavirus", "415354003", "Rotavirus vaccination"),
+    MEN_B("MenB", "720537002", "Meningitis B vaccination"),
+    MEN_ASWY("MenACWY", "390892002", "Meningitis ACW & Y vaccination"),
+    VERICELLA("Varicella", "571611000119101", "Administration of varicella live vaccine"),
+    HIB_MENC("Hib/MenC", "428975001", "Vaccination with Haemophilus influenzae type B and Neisseria meningitidis serotype C combination vaccine"),
+    FLU("Flu", "86198006", "Influenza vaccination"),
+    HPV("HPV", "428570002", "Vaccination for human papillomavirus");
 
     private String name;
-    ImmunisationCodelist(String name) { this.name = name; }
-    public String getName() { return this.name; }
-    public String getId() { return this.name(); }
+    private String code;
+    private String description;
+
+    ImmunisationCodelist(String name, String code, String description) {
+        this.name = name;
+        this.code = code;
+        this.description = description;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getId() {
+        return this.name();
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
