5446ceb9b180e782da6bc1ccdb5bc4ed9e15504a
Pavlo Maks <pavlo@solidstategroup.com>
2020-01-28 15:24:57 +0000

    mapped dose frequency

--- root/common/src/main/java/org/patientview/persistence/model/enums/DoseFrequencyTypes.java
+++ root/common/src/main/java/org/patientview/persistence/model/enums/DoseFrequencyTypes.java
@@ -4,13 +4,28 @@ package org.patientview.persistence.model.enums;
  * Enum for relapse medication's dose frequency types.
  */
 public enum DoseFrequencyTypes {
-    ONE_DAY("Once a day"),
-    TWO_DAY("2 a day"),
-    THREE_DAY("3 a day"),
-    FOUR_DAY("4 a day");
+    ONE_DAY("Once a day", "1/d"),
+    TWO_DAY("2 a day", "2/d"),
+    THREE_DAY("3 a day", "3/d"),
+    FOUR_DAY("4 a day", "4/d");
 
     private String name;
-    DoseFrequencyTypes(String name) { this.name = name; }
-    public String getName() { return this.name; }
-    public String getId() { return this.name(); }
+    private String value;
+
+    DoseFrequencyTypes(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getId() {
+        return this.name();
+    }
 }
