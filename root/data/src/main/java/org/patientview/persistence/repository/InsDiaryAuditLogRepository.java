88758a38e344c4e9ce0682c2ffd6b11398ea8b6a
Pavlo Maks <pavlo@solidstategroup.com>
2020-01-29 14:18:02 +0000

    ins audit log entity and repo

---
+++ root/data/src/main/java/org/patientview/persistence/repository/InsDiaryAuditLogRepository.java
@@ -0,0 +1,26 @@
+package org.patientview.persistence.repository;
+
+import org.patientview.persistence.model.InsDiaryAuditLog;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ *
+ */
+@Repository
+public interface InsDiaryAuditLogRepository extends CrudRepository<InsDiaryAuditLog, Long> {
+
+    List<InsDiaryAuditLog> findAll();
+
+    @Query("SELECT a InsDiaryAuditLog a WHERE patientId = :patientId")
+    List<InsDiaryAuditLog> findByPatientId(@Param("patientId") Long patientId);
+
+    @Modifying(clearAutomatically = true)
+    @Query("DELETE InsDiaryAuditLog WHERE patientId = :patientId")
+    void deleteByPatientId(@Param("patientId") Long patientId);
+}
