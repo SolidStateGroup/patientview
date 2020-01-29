fa56818f1c4e1be1f2212d5521938c253167f386
Pavlo Maks <pavlo@solidstategroup.com>
2020-01-29 17:27:10 +0000

    code refactor

--- root/api/src/main/java/org/patientview/api/job/INSDiaryUkrdcTask.java
+++ root/api/src/main/java/org/patientview/api/job/INSDiaryUkrdcTask.java
@@ -59,7 +59,7 @@ public class INSDiaryUkrdcTask {
     /**
      * Builds XMLs fpr INS diary entries and queue them up to be send to UKRDC
      */
-//    @Scheduled(cron = "0 */5 * * * ?") // every 5 minutes
+    // @Scheduled(cron = "0 */5 * * * ?") // every 5 minutes
     @Scheduled(cron = "0 0 1 * * ?") // every day at 1:00AM
     @Transactional
     public void insDiaryQueue() throws ResourceNotFoundException {
