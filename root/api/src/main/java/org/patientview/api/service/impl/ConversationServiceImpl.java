48caccf1ca6b3ebc50e8c572c4b20b279c3b38df
Pavlo Maks <pavlo@solidstategroup.com>
2021-06-15 18:55:24 +0100

    cleanup

--- root/api/src/main/java/org/patientview/api/service/impl/ConversationServiceImpl.java
+++ root/api/src/main/java/org/patientview/api/service/impl/ConversationServiceImpl.java
@@ -88,7 +88,6 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Properties;
 import java.util.Set;
 
